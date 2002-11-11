3cb0f142613547d83ddbec7e8b1600e0aa9cccfc
heijnen <heijnen>
2002-11-11 14:05:36 +0000

    Brought Box3D up-to-date (fields changed to private, getters added, constructors removed, generateTexture->setTexture).

--- java/ab/j3d/model/Box3D.java
+++ java/ab/j3d/model/Box3D.java
@@ -9,6 +9,7 @@ package common.renderer;
  * form without express permission from Numdata BV. Please contact Numdata BV
  * for license information.
  */
+import java.util.Stack;
 import java.util.Hashtable;
 import common.db.TextureSpec;
 import common.model.Matrix3D;
@@ -22,65 +23,12 @@ import common.model.Matrix3D;
 public class Box3D
 	extends Object3D
 {
-	public final Matrix3D xform;
-	public final float   dx;
-	public final float   dy;
-	public final float   dz;
-
-
-	public final static int X_POS = 0;
-	public final static int X_NEG = 1;
-	public final static int Y_POS = 2;
-	public final static int Y_NEG = 3;
-	public final static int Z_POS = 4;
-	public final static int Z_NEG = 5;
-
+	private Matrix3D _xform;
+	private float _dx;
+	private float _dy;
+	private float _dz;
 	/**
-	 * Constructor for box object.
-	 *
-	 * @param	xform	Transform for vertices of object.
-	 * @param	dx		Width of box (x-axis).
-	 * @param	dy		Height of box (y-axis).
-	 * @param	dz		Depth of box (z-axis).
-	 */
-	public Box3D( final Matrix3D xform , final float dx , final float dy , final float dz )
-	{
-		this( xform , dx , dy , dz , null , null );
-	}
-
-	/**
-	 * Constructor for box object.
-	 *
-	 * @param	xform				Transformation to apply to all vertices of the box.
-	 * @param	dx					Width of box (x-axis).
-	 * @param	dy					Height of box (y-axis).
-	 * @param	dz					Depth of box (z-axis).
-	 * @param	frontalTexture		Texture to use for front and back (Y-axis).
-	 * @param	flipFrontal			Rotate texture for front and back 90 degrees.
-	 * @param	verticalTexture		Texture to use for left and right (X-axis).
-	 * @param	flipVertical		Rotate texture for left and right 90 degrees.
-	 * @param	horizontalTexture	Texture to use for top and bottom (Z-axis).
-	 * @param	flipHorizontal		Rotate texture for top and bottom 90 degrees.
-	 */
-	public Box3D( final Matrix3D xform , final float dx , final float dy , final float dz , 
-		TextureSpec frontalTexture    , boolean flipFrontal ,
-		TextureSpec verticalTexture   , boolean flipVertical ,
-		TextureSpec horizontalTexture , boolean flipHorizontal )
-	{
-		this.xform = xform;
-		this.dx    = dx;
-		this.dy    = dy;
-		this.dz    = dz;
-
-		
-		generate( 
-			frontalTexture , flipFrontal ,
-			verticalTexture , flipVertical ,
-			horizontalTexture , flipHorizontal );
-	}
-
-	/**
-	 * Constructor for box object.
+	 * Set box properties.
 	 *
 	 * @param	xform		Transformation to apply to all vertices of the box.
 	 * @param	dx			Width of box (x-axis).
@@ -88,98 +36,47 @@ public class Box3D
 	 * @param	dz			Depth of box (z-axis).
 	 * @param	mainTexture	Main texture of box.
 	 * @param	sideTexture	Texture for sides of box.
+	 *
+	 * @return	This Box3D instance.
 	 */
 	public Box3D( final Matrix3D xform , final float dx , final float dy , final float dz , final TextureSpec mainTexture , final TextureSpec sideTexture )
 	{
-		this.xform = xform;
-		this.dx    = dx;
-		this.dy    = dy;
-		this.dz    = dz;
+		_xform = xform;
+		_dx    = dx;
+		_dy    = dy;
+		_dz    = dz;
 
-		float ax = Math.abs( dx );
-		float ay = Math.abs( dy );
-		float az = Math.abs( dz );
-		
 		/*
-		 * Frontal (delta-Y is smallest)
+		 * Set frontal, vertical, and horizontal face properties.
 		 */
-		if ( ay < ax && ay < az )
-			generate( mainTexture , false , sideTexture , false , sideTexture , true );
-			
-		/*
-		 * Vertical (delta-X is smallest)
-		 */
-		else if ( ax < ay && ax < az )
-			generate( sideTexture , false , mainTexture , false , sideTexture , false );
-			
-		/*
-		 * Horizontal (delta-Z is smallest)
-		 */
-		else //if ( az < ax && az < ay )
-			generate( sideTexture , true , sideTexture , true , mainTexture , true );
-	}
+		final float ax = Math.abs( dx );
+		final float ay = Math.abs( dy );
+		final float az = Math.abs( dz );
+		final boolean isFrontal    = ( ay < ax && ay < az );
+		final boolean isVertical   = ( ax < ay && ax < az );
+		final boolean isHorizontal = ( az < ax && az < ay );
 
-	/**
-	 * Constructor for box object.
-	 *
-	 * @param	x			Base position on X-axis.
-	 * @param	y			Base position on Y-axis.
-	 * @param	z			Base position on Z-axis.
-	 * @param	dx			Width of box (x-axis).
-	 * @param	dy			Height of box (y-axis).
-	 * @param	dz			Depth of box (z-axis).
-	 * @param	texture		Texture to use for all sides of the 3D box.
-	 */
-	public Box3D( final float x , final float y , final float z , final float dx , final float dy , final float dz , TextureSpec texture )
-	{
-		this( Matrix3D.INIT.plus( x , y , z ) , dx , dy , dz , texture , texture );
-	}
-
-	/**
-	 * Generate Object3D properties.
-	 *
-	 * @param	frontalTexture		Texture to use for front and back (Y-axis).
-	 * @param	flipFrontal			Rotate texture for front and back 90 degrees.
-	 * @param	verticalTexture		Texture to use for left and right (X-axis).
-	 * @param	flipVertical		Rotate texture for left and right 90 degrees.
-	 * @param	horizontalTexture	Texture to use for top and bottom (Z-axis).
-	 * @param	flipHorizontal		Rotate texture for top and bottom 90 degrees.
-	 */
-	public void generate( 
-		TextureSpec frontalTexture    , boolean flipFrontal ,
-		TextureSpec verticalTexture   , boolean flipVertical ,
-		TextureSpec horizontalTexture , boolean flipHorizontal )
-	{
-		/*
-		 * Check if any face has a texture image.
-		 */
-		final boolean hasTextureImage =
-			( frontalTexture    != null && frontalTexture   .isTexture() ) ||
-			( verticalTexture   != null && verticalTexture  .isTexture() ) ||
-			( horizontalTexture != null && horizontalTexture.isTexture() );
-			
-		
 		/*
 		 * Create vertices;
 		 */
-		float[] vertices =
+		final float[] vertices =
 		{
-			0  , 0  , 0  , // 0
-			dx , 0  , 0  , // 1
-			dx , dy , 0  , // 2
-			0  , dy , 0  , // 3
-			0  , 0  , dz , // 4
-			dx , 0  , dz , // 5
-			dx , dy , dz , // 6
-			0  , dy , dz   // 7
+			  0 ,   0 ,   0 , // 0
+			_dx ,   0 ,   0 , // 1
+			_dx , _dy ,   0 , // 2
+			  0 , _dy ,   0 , // 3
+			  0 ,   0 , _dz , // 4
+			_dx ,   0 , _dz , // 5
+			_dx , _dy , _dz , // 6
+			  0 , _dy , _dz   // 7
 		};
-		
+
 		xform.transform( vertices , vertices , 8 );
 
 		/*
 		 * Create faces
 		 */
-		int[][] faceVert =
+		final int[][] faceVert =
 		{
 			{ 0 , 4 , 5 , 1 } , // 0 - front
 			{ 2 , 6 , 7 , 3 } , // 1 - back
@@ -188,8 +85,18 @@ public class Box3D
 			{ 4 , 7 , 6 , 5 } , // 4 - top
 			{ 1 , 2 , 3 , 0 }   // 5 - bottom
 		};
-		
-		TextureSpec[] faceMat =
+
+		/*
+		 * Set texture properties.
+		 */	
+		final TextureSpec frontalTexture    = isFrontal ? mainTexture : sideTexture;
+		final boolean     flipFrontal       = isHorizontal;
+		final TextureSpec verticalTexture   = isVertical ? mainTexture : sideTexture;
+		final boolean     flipVertical      = isHorizontal;
+		final TextureSpec horizontalTexture = isHorizontal ? mainTexture : sideTexture;
+		final boolean     flipHorizontal    = !isVertical;
+		 
+		final TextureSpec[] faceMat =
 		{
 			frontalTexture ,
 			frontalTexture ,
@@ -201,28 +108,49 @@ public class Box3D
 
 		int[][] faceTU = null;
 		int[][] faceTV = null;
-		if ( hasTextureImage )
+		if ( ( mainTexture != null && mainTexture.isTexture() )
+		  || ( sideTexture != null && sideTexture.isTexture() ) )
 		{
 			faceTU = new int[ 6 ][];
 			faceTV = new int[ 6 ][];
 			
-			generateTexture( frontalTexture , flipFrontal , 0 , 
-				faceTU , xform.xo , xform.xo + dx ,
-				faceTV , xform.zo , xform.zo + dz );
+			setTexture( frontalTexture , flipFrontal , 0 , 
+				faceTU , xform.xo , xform.xo + _dx ,
+				faceTV , xform.zo , xform.zo + _dz );
 
-			generateTexture( verticalTexture , flipVertical , 2 , 
-				faceTU , xform.yo , xform.yo + dy ,
-				faceTV , xform.zo , xform.zo + dz );
+			setTexture( verticalTexture , flipVertical , 2 , 
+				faceTU , xform.yo , xform.yo + _dy ,
+				faceTV , xform.zo , xform.zo + _dz );
 
-			generateTexture( horizontalTexture , flipHorizontal , 4 , 
-				faceTU , xform.xo , xform.xo + dx , 
-				faceTV , xform.yo , xform.yo + dy );
+			setTexture( horizontalTexture , flipHorizontal , 4 , 
+				faceTU , xform.xo , xform.xo + _dx , 
+				faceTV , xform.yo , xform.yo + _dy );
 		}
 
 		/*
 		 * Set Object3D properties.
 		 */
 		set( vertices , faceVert , faceMat , faceTU , faceTV , false );
+	}
+
+	public float getDX()
+	{
+		return _dx;
+	}
+
+	public float getDY()
+	{
+		return _dy;
+	}
+
+	public float getDZ()
+	{
+		return _dz;
+	}
+
+	public Matrix3D getTransform()
+	{
+		return _xform;
 	}
 
 	/**
@@ -238,7 +166,7 @@ public class Box3D
 	 * @param	tv1			Start V coordinate.
 	 * @param	tv2			End V coordinate.
 	 */
-	public void generateTexture( 
+	protected static void setTexture( 
 		final TextureSpec texture , final boolean flipUV , final int index , 
 		final int[][] faceTU , final float tu1 , final float tu2 , 
 		final int[][] faceTV , final float tv1 , final float tv2 )
