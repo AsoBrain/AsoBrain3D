306a1fbcccd5e2f418f9bfe8178285f3f9a3d645
heijnen <heijnen>
2003-02-16 13:51:33 +0000

    Goal compliance fixes. Using CommonTools.tokenize() instead of StringTokenizer. Replaced static Vector3D.length( vector ) with non-static length() method.

--- java/ab/j3d/Vector3D.java
+++ java/ab/j3d/Vector3D.java
@@ -3,29 +3,28 @@ package common.model;
 /*
  * $Id$
  *
- * (C) Copyright Numdata BV 2000-2002 - All Rights Reserved
- * (C) Copyright Peter S. Heijnen 1999-2002 - All Rights Reserved
+ * (C) Copyright Numdata BV 2000-2003 - All Rights Reserved
+ * (C) Copyright Peter S. Heijnen 1999-2003 - All Rights Reserved
  *
  * This software may not be used, copyied, modified, or distributed in any
  * form without express permission from Numdata BV or Peter S. Heijnen. Please
  * contact Numdata BV or Peter S. Heijnen for license information.
  */
-import java.util.StringTokenizer;
+import common.CommonTools;
 
 /**
  * This class represents a 3D vector. It is used by ROM, but can also
  * be used for other purposes (it does not depend on ROM classes).
  *
  * @author	Peter S. Heijnen
- *
  * @version	$Revision$ ($Date$, $Author$)
  */
 public final class Vector3D
 {
-	public final float x;
-	public final float y;
-	public final float z;
-	
+	/** X component of 3D vector. */ public final float x;
+	/** Y component of 3D vector. */ public final float y;
+	/** Z component of 3D vector. */ public final float z;
+
 	/**
 	 * Initial value of a vector (0-vector).
 	 */
@@ -37,7 +36,7 @@ public final class Vector3D
 	 * @param	ny	Y-coordinate of vector.
 	 * @param	nz	Z-coordinate of vector.
 	 */
-	private Vector3D( float nx , float ny , float nz )
+	private Vector3D( final float nx , final float ny , final float nz )
 	{
 		x = nx;
 		y = ny;
@@ -52,7 +51,7 @@ public final class Vector3D
 	 *
 	 * @return	angle between vectors in radians.
 	 */
-	public static float angle( Vector3D v1 , Vector3D v2 )
+	public static float angle( final Vector3D v1 , final Vector3D v2 )
 	{
 		return (float)Math.acos( cosAngle( v1 , v2 ) );
 	}
@@ -65,9 +64,9 @@ public final class Vector3D
 	 *
 	 * @return	cos(angle) between vectors.
 	 */
-	public static float cosAngle( Vector3D v1 , Vector3D v2 )
+	public static float cosAngle( final Vector3D v1 , final Vector3D v2 )
 	{
-		float l = length( v1 ) * length( v2 );
+		final float l = v1.length() * v2.length();
 		if ( l == 0f ) return( 0f );
 		return( dot( v1 , v2 ) / l );
 	}
@@ -80,7 +79,7 @@ public final class Vector3D
 	 *
 	 * @return	Resulting vector.
 	 */
-	public static Vector3D cross( Vector3D v1 , Vector3D v2 )
+	public static Vector3D cross( final Vector3D v1 , final Vector3D v2 )
 	{
 		return v1.set( v1.y * v2.z - v1.z * v2.y ,
 					   v1.z * v2.x - v1.x * v2.z ,
@@ -88,16 +87,18 @@ public final class Vector3D
 	}
 
 	/**
-	 * Calculate length of vector.
+	 * Calculate distance between this and another vector (both should be absolute).
 	 *
-	 * @return	Length of vector.
+	 * @param   other   Vector to calculate the distance to.
+	 *
+	 * @return	Distance between this and the specified other vector.
 	 */
-	public float distanceTo( Vector3D other )
+	public float distanceTo( final Vector3D other )
 	{
 		final float dx = x - other.x;
 		final float dy = y - other.y;
 		final float dz = z - other.z;
-		
+
 		return (float)Math.sqrt( dx * dx + dy * dy + dz * dz );
 	}
 
@@ -110,7 +111,7 @@ public final class Vector3D
 	 *
 	 * @return	Dot product.
 	 */
-	public static float dot( Vector3D v1 , Vector3D v2 )
+	public static float dot( final Vector3D v1 , final Vector3D v2 )
 	{
 		return( v1.x * v2.x + v1.y * v2.y + v1.z * v2.z );
 	}
@@ -122,10 +123,10 @@ public final class Vector3D
 	 * @param	y	Y-coordinate of vector.
 	 * @param	z	Z-coordinate of vector.
 	 *
-	 * @return	<code>true</code> if vectors are equal,
-	 *		<code>false</code> if not.
+	 * @return	<CODE>true</CODE> if vectors are equal,
+	 *	    	<CODE>false</CODE> if not.
 	 */
-	public boolean equals( float x , float y , float z )
+	public boolean equals( final float x , final float y , final float z )
 	{
 		return( ( x != x /* => NaN*/ || x == this.x ) &&
 				( y != y /* => NaN*/ || y == this.y ) &&
@@ -137,15 +138,16 @@ public final class Vector3D
 	 *
 	 * @param	other	Vector to compare with.
 	 *
-	 * @return	<code>true</code> if vectors are equal,
-	 *		<code>false</code> if not.
+	 * @return	<CODE>true</CODE> if vectors are equal,
+	 *  		<CODE>false</CODE> if not.
 	 */
-	public boolean equals( Object other )
+	public boolean equals( final Object other )
 	{
 		if ( other == this ) return true;
 		if ( other == null ) return false;
 		if ( !( other instanceof Vector3D ) ) return false;
-		Vector3D v = (Vector3D)other;
+
+		final Vector3D v = (Vector3D)other;
 		return( x == v.x && y == v.y && z == v.z );
 	}
 
@@ -157,14 +159,13 @@ public final class Vector3D
 	 *
 	 * @return	Object instance.
 	 */
-	public static Vector3D fromString( String value )
+	public static Vector3D fromString( final String value )
 	{
-		StringTokenizer st = new StringTokenizer( value , "," );
+		final String[] tokens = CommonTools.tokenize( value , ',' );
 		
-		return Vector3D.INIT.set(
-			Float.valueOf( st.nextToken() ).floatValue() ,
-			Float.valueOf( st.nextToken() ).floatValue() ,
-			Float.valueOf( st.nextToken() ).floatValue() );
+		return Vector3D.INIT.set( new Float( tokens[ 0 ] ).floatValue() ,
+		                          new Float( tokens[ 1 ] ).floatValue() ,
+		                          new Float( tokens[ 2 ] ).floatValue() );
 	}
 
 	/**
@@ -172,9 +173,9 @@ public final class Vector3D
 	 *
 	 * @return	Length of vector.
 	 */
-	public static float length( Vector3D vector )
+	public float length()
 	{
-		return (float)Math.sqrt( vector.x * vector.x + vector.y * vector.y + vector.z * vector.z );
+		return (float)Math.sqrt( x * x + y * y + z * z );
 	}
 
 	/**
@@ -184,7 +185,7 @@ public final class Vector3D
 	 *
 	 * @return	Resulting vector.
 	 */
-	public Vector3D minus( Vector3D other )
+	public Vector3D minus( final Vector3D other )
 	{
 		return minus( other.x , other.y , other.z );
 	}
@@ -198,7 +199,7 @@ public final class Vector3D
 	 *
 	 * @return	Resulting vector.
 	 */
-	public Vector3D minus( float x , float y , float z )
+	public Vector3D minus( final float x , final float y , final float z )
 	{
 		return set( this.x - x , this.y - y , this.z - z );
 	}
@@ -210,7 +211,7 @@ public final class Vector3D
 	 *
 	 * @return	Resulting vector.
 	 */
-	public Vector3D multiply( float factor )
+	public Vector3D multiply( final float factor )
 	{
 		return set( x * factor , y * factor , z * factor );
 	}
@@ -222,7 +223,7 @@ public final class Vector3D
 	 *
 	 * @return	Resulting vector.
 	 */
-	public Vector3D plus( Vector3D other )
+	public Vector3D plus( final Vector3D other )
 	{
 		return plus( other.x , other.y , other.z );
 	}
@@ -236,7 +237,7 @@ public final class Vector3D
 	 *
 	 * @return	Resulting vector.
 	 */
-	public Vector3D plus( float x , float y , float z )
+	public Vector3D plus( final float x , final float y , final float z )
 	{
 		return set( this.x + x , this.y + y , this.z + z );
 	}
@@ -255,13 +256,13 @@ public final class Vector3D
 		if ( x != x /* => NaN*/ ) x = this.x;
 		if ( y != y /* => NaN*/ ) y = this.y;
 		if ( z != z /* => NaN*/ ) z = this.z;
-		
+
 		if ( x == 0f && y == 0f && z == 0f )
 			return( INIT );
-		
+
 		if ( x == this.x && y == this.y && z == this.z )
 			return( this );
-		
+
 		return new Vector3D( x , y , z );
 	}
 
