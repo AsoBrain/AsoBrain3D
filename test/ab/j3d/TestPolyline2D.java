2c400ccfe7c2ad46f0cf8b8450003d99012c4d24
heijnen <heijnen>
2003-05-07 10:22:41 +0000

    BUG:        RuntimeException: Linesegment cannot have two intersections and end in convex shape. Symptom:    Large panels cannot have an connection to each other. (i.e. back and ceiling) Analysis:   The intersection between the connected surfaces is calculated using             PolyLine2D. In this class intersections are calculated per line             segment and checked if they are 'inside' or 'outside' of the             intersection. If two intersections are found, the start and end point             are compared to the last found point, to see if the line segment is             connected. This test is performed using the             <CODE>PolyPoint2D.almostEquals( PolyPoint2D , tolerance )</CODE>             method to cope with rounding errors. If the X or Y variable is larger             than 2047 mm, the check mysteriously? Further analysis showed that             the problem has to do with the maximum number of digits in a             <CODE>float</CODE> and the method used to handle the tolerance. To             compare some value (X) to another value (Y), the tolerance (0.0001)             was added to and subtracted from X before comparing it to Y.
                         0-------*-------0
                     X-0.0001f < X < X+0.0001f
    
                This, however, causes trouble with large values of X due to the lack
                of available digits (2048 +/- 0.0001 needs more digits than a float
                can handle), and therefore effectively eliminating the tolerance value .
    Solution:   Instead of adding/subtracting the tolerance to one of the value, the
                text is performed against the difference between X and Y, which should
                be between -tolerance and +tolerance.
    
    General updates to unit test classes:
     - SodaTestCase is no longer a test base class, but a tool box;
     - Tests may now write messages to stdout. This may not be desirable, but makes life just a bit easier;
     - Added CLASS_NAME constant to refer to the test case;
     - Added methods context messages to individual test methods;
     - Removed main() method from tests, since both IDEA and ANT have direct JUnit support.
    
    Various updates to source file layout, etc. This is the first release that builds with ANT.

--- test/ab/j3d/TestPolyline2D.java
+++ test/ab/j3d/TestPolyline2D.java
@@ -1,5 +1,3 @@
-package com.numdata.soda.backoffice;
-
 /*
  * $Id$
  *
@@ -9,10 +7,9 @@ package com.numdata.soda.backoffice;
  * form without express permission from Numdata BV. Please contact Numdata BV
  * for license information.
  */
-import com.numdata.soda.common.SodaTestCase;
+package com.numdata.soda;
 
-import com.numdata.soda.backoffice.PolyPoint2D;
-import com.numdata.soda.backoffice.Polyline2D;
+import junit.framework.TestCase;
 
 /**
  * Test functionality of Polyline2D class.
@@ -23,13 +20,20 @@ import com.numdata.soda.backoffice.Polyline2D;
  * @version	$Revision$ ($Date$, $Author$)
  */
 public final class TestPolyline2D
-	extends SodaTestCase
+	extends TestCase
 {
+	/**
+	 * Name of this class.
+	 */
+	private static final String CLASS_NAME = TestPolyline2D.class.getName();
+
 	/**
 	 * Test constructor for rectangular polyline.
 	 */
 	public static void testRectangleConstructor()
 	{
+		System.out.println( CLASS_NAME + ".testRectangleConstructor()" );
+
 		final Polyline2D pl = new Polyline2D( 123 , 456 );
 		assertEquals( "rectangle should have 5 points!" , 5 , pl.getPointCount() );
 
@@ -45,6 +49,8 @@ public final class TestPolyline2D
 	 */
 	public static void testHorizontalLineConstructor()
 	{
+		System.out.println( CLASS_NAME + ".testHorizontalLineConstructor()" );
+
 		final Polyline2D pl = new Polyline2D( 123 , 0 );
 		assertEquals( "horizontal line should have 2 points" , 2 , pl.getPointCount() );
 
@@ -57,10 +63,128 @@ public final class TestPolyline2D
 	 */
 	public static void testVerticalLineConstructor()
 	{
+		System.out.println( CLASS_NAME + ".testVerticalLineConstructor()" );
+
 		final Polyline2D pl = new Polyline2D( 0 , 456 );
 		assertEquals( "vertical line should have 2 points" , 2 , pl.getPointCount() );
 
 		assertEquals( "vertical line[ 0 ] is off" , new PolyPoint2D( 0 ,   0 ) , pl.getPoint( 0 ) );
 		assertEquals( "vertical line[ 1 ] is off" , new PolyPoint2D( 0 , 456 ) , pl.getPoint( 1 ) );
+	}
+
+	/**
+	 * Test getIntersection for two rectangular polylines.
+	 * In this test, no intersection should be found.
+	 */
+	public static void testGetIntersection1()
+	{
+		System.out.println( CLASS_NAME + ".testGetIntersection1()" );
+
+		final Polyline2D pl1 = new Polyline2D();
+		pl1.append(     0 ,     0 );
+		pl1.append(  1000 ,     0 );
+		pl1.append(  1000 ,    18 );
+		pl1.append(     0 ,    18 );
+		pl1.append(     0 ,     0 );
+
+		final Polyline2D pl2 = new Polyline2D();
+		pl2.append(     0 ,    19 );
+		pl2.append(  2000 ,    19 );
+		pl2.append(  2000 ,    39 );
+		pl2.append(     0 ,    39 );
+		pl2.append(     0 ,    19 );
+
+		final Polyline2D interSection = pl1.getIntersection(  pl2 );
+		assertTrue( "Intersection found! (but no intersection is possible)" , interSection == null );
+	}
+
+	/**
+	 * Test getIntersection for two rectangular polylines.
+	 * In this test, an intersection should be found.
+	 */
+	public static void testGetIntersection2()
+	{
+		System.out.println( CLASS_NAME + ".testGetIntersection2()" );
+
+		final Polyline2D pl1 = new Polyline2D();
+		pl1.append(     0 ,     0 );
+		pl1.append(  2047 ,     0 );
+		pl1.append(  2047 ,    18 );
+		pl1.append(     0 ,    18 );
+		pl1.append(     0 ,     0 );
+
+		final Polyline2D pl2 = new Polyline2D();
+		pl2.append(     0 ,  -363 );
+		pl2.append(  2047 ,  -363 );
+		pl2.append(  2047 ,    35 );
+		pl2.append(     0 ,    35 );
+		pl2.append(     0 ,  -363 );
+
+		final Polyline2D interSection = pl1.getIntersection(  pl2 );
+		assertTrue( "No intersection found! (normal x, y values)" , interSection != null );
+	}
+
+	/**
+	 * Test getIntersection for two rectangular polylines.
+	 * In this test, two intersections should be found.
+ 	 * <P>
+	 * RELATED BUGS:
+	 * <UL>
+	 *  <LI>
+	 *    <B>BUG:</B><BR>
+	 *    RuntimeException: Linesegment cannot have two intersections and end in convex shape.
+	 *    <BR>
+	 *    <B>Symptom:</B><BR>
+ 	 *    Large panels cannot have an connection to each other. (i.e. back and ceiling)
+	 *    <BR>
+	 *    <B>Analysis:</B><BR>
+	 *    The intersection between the connected surfaces is calculated using
+	 *    PolyLine2D. In this class intersections are calculated per line
+	 *    segment and checked if they are 'inside' or 'outside' of the
+	 *    intersection. If two intersections are found, the start and end point
+	 *    are compared to the last found point, to see if the line segment is
+	 *    connected. This test is performed using the
+	 *    <CODE>PolyPoint2D.almostEquals( PolyPoint2D , tolerance )</CODE>
+	 *    method to cope with rounding errors. If the X or Y variable is larger
+	 *    than 2047 mm, the check mysteriously? Further analysis showed that
+	 *    the problem has to do with the maximum number of digits in a
+	 *    <CODE>float</CODE> and the method used to handle the tolerance. To
+	 *    compare some value (X) to another value (Y), the tolerance (0.0001)
+	 *    was added to and subtracted from X before comparing it to Y.
+	 *    <PRE>
+	 *         0-------*-------0
+	 *     X-0.0001f < X < X+0.0001f
+	 *    </PRE>
+	 *    This, however, causes trouble with large values of X due to the lack
+	 *    of available digits (2048 +/- 0.0001 needs more digits than a float
+	 *    can handle), and therefore effectively eliminating the tolerance value .
+	 *    <BR>
+	 *    <B>Solution:</B><BR>
+	 *    Instead of adding/subtracting the tolerance to one of the value, the
+	 *    text is performed against the difference between X and Y, which should
+	 *    be between -tolerance and +tolerance.
+	 *  </LI>
+	 * </UL>
+	 */
+	public static void testGetIntersection3()
+	{
+		System.out.println( CLASS_NAME + ".testGetIntersection3()" );
+
+		final Polyline2D pl1 = new Polyline2D();
+		pl1.append(     0 ,     0 );
+		pl1.append(  4000 ,     0 );
+		pl1.append(  4000 ,    18 );
+		pl1.append(     0 ,    18 );
+		pl1.append(     0 ,     0 );
+
+		final Polyline2D pl2 = new Polyline2D();
+		pl2.append(     0 ,  -363 );
+		pl2.append(  4000 ,  -363 );
+		pl2.append(  4000 ,    35 );
+		pl2.append(     0 ,    35 );
+		pl2.append(     0 ,  -363 );
+
+		final Polyline2D interSection = pl1.getIntersection(  pl2 );
+		assertTrue( "No intersection found! (large x value)" , interSection != null );
 	}
 }
