502107a5d3ca120d6a926533ef500c7a6193375e
gerwin <gerwin>
2004-11-05 10:59:24 +0000

    * Changed the resulting movements of the 'from point' when dragging:   - Left mouse button:     * x-movement: Rotate around the 'to point'.     * y movement: 'Fly over' the 'to point'.   - Middle mouse button:     * x-movement: Strafe left and right.     * y-movement: Strafe up and down.   - Right mouse button:     * y-movement: Zoom in and out.

--- java/ab/j3d/view/FromToViewControl.java
+++ java/ab/j3d/view/FromToViewControl.java
@@ -48,6 +48,11 @@ public final class FromToViewControl
 	extends ViewControl
 {
 	/**
+	 * Name of this class.
+	 */
+	private static final String CLASS_NAME = FromToViewControl.class.getName();
+
+	/**
 	 * Point to look from.
 	 */
 	private Vector3D _from;
@@ -333,17 +338,19 @@ public final class FromToViewControl
 	{
 		final Vector3D upPrimary = _upPrimary;
 		final Vector3D from      = _dragStartFrom;
+		final Vector3D to        = _dragStartTo;
 		final Matrix3D transform = _dragStartTransform;
 
-		final double deltaX = event.getDeltaUnitX();
-		final double deltaY = event.getDeltaUnitY();
+		final double   deltaX    = -event.getDeltaRadX();
+		final double   deltaY    =  event.getDeltaUnitY();
 
-		final Vector3D zAxis = Vector3D.INIT.set( transform.zx , transform.zy , transform.zz );
-		final Vector3D xAxis = Vector3D.cross( zAxis , upPrimary );
-		final Vector3D yAxis = Vector3D.cross( upPrimary , xAxis );
+		final Vector3D zAxis     = Vector3D.INIT.set( transform.zx , transform.zy , transform.zz );
+		final Vector3D xAxis     = Vector3D.cross( zAxis , upPrimary );
+		final Vector3D yAxis     = Vector3D.cross( upPrimary , xAxis );
+		final Matrix3D rotation  = Matrix3D.getRotationTransform( to , upPrimary , deltaX );
 
 		Vector3D newFrom = from;
-		newFrom = newFrom.plus( xAxis.multiply( deltaX ) );
+		newFrom = rotation.multiply( newFrom );
 		newFrom = newFrom.plus( yAxis.multiply( deltaY ) );
 		setFrom( newFrom );
 	}
@@ -352,16 +359,17 @@ public final class FromToViewControl
 	{
 		final Vector3D upPrimary = _upPrimary;
 		final Vector3D from      = _dragStartFrom;
-		final Vector3D to        = _dragStartTo;
+		final Matrix3D transform = _dragStartTransform;
 
-		final double deltaX = -event.getDeltaRadX();
-		final double deltaY = -event.getDeltaUnitY();
+		final double   deltaX    =  event.getDeltaUnitX();
+		final double   deltaY    = -event.getDeltaUnitY();
 
-		final Matrix3D rotation  = Matrix3D.getRotationTransform( to , upPrimary , deltaX );
+		final Vector3D zAxis     = Vector3D.INIT.set( transform.zx , transform.zy , transform.zz );
+		final Vector3D xAxis     = Vector3D.cross( zAxis , upPrimary );
 		final Vector3D elevation = upPrimary.multiply( deltaY );
 
 		Vector3D newFrom = from;
-		newFrom = rotation.multiply( newFrom );
+		newFrom = newFrom.plus( xAxis.multiply( deltaX ) );
 		newFrom = newFrom.plus( elevation );
 		setFrom( newFrom );
 	}
