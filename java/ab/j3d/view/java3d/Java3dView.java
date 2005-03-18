77049524593c7c7890a25311891a74a35cb06e6c
heijnen <heijnen>
2005-03-18 00:32:20 +0000

    Optimized 'Java3dView.update()' so that it does not create any temporary objects (IMPORTANT: this causes concurrency problems if 'update()' is called from multiple threads).
    Make 'Java3dUniverse' extend 'VirtualUniverse' (javax.media.j3d) instead of 'SimpleUniverse' (com.sun.j3d.utils.universe):
     - Reasoning:
        > A 'SimpleUniverse' always creates a 'Canvas3D' (Visible! => Very annoying).
        > The interface is not meant for multiple views.
        > Makes is very difficult from using classes outside the 'com.sun.j3d.utils.universe' package.
     - Implementation:
        > Add functionality to 'Java3dUniverse' that was previously available in 'SimpleUniverse' and related classes (like 'Viewer' and 'ViewingPlatform'):
           - Added 'locale' property (read-only , created during construction).
           - Added 'physicalBody' property (read-only , created on-demand by getter).
           - Added 'physicalEnvironment' property (read-only , created on-demand by getter).
           - Added 'addBranchGroup()' method (replaced 'SimpleUniverse.addBranchGraph()', which had strange documented semantics).
           - Added 'createView()' method to create a view branch in the universe (replaces 'Viewer').
        > Restructured 'Java3dView' internally to use 'Java3dUniverse.createView()' to create views instead of the old 'Viewer' class.
        > Made 'Java3dTools.createCanvas3D()' independent from 'SimpleUniverse.createCanvas3D()' (actually used by 'Java3dUniverse' now to create a canvas, if needed).
        > Since no initial 'Canvas3D' is used, constructor arguments for supplying one have been removed.
        > Removed methods that were overriding 'SimpleUniverse' methods or exposed its functionality.

--- java/ab/j3d/view/java3d/Java3dView.java
+++ java/ab/j3d/view/java3d/Java3dView.java
@@ -21,14 +21,11 @@ package ab.j3d.view.java3d;
 
 import java.awt.Component;
 import javax.media.j3d.Canvas3D;
-import javax.media.j3d.Group;
+import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
 import javax.media.j3d.View;
-import javax.media.j3d.Locale;
-
-import com.sun.j3d.utils.universe.MultiTransformGroup;
-import com.sun.j3d.utils.universe.Viewer;
-import com.sun.j3d.utils.universe.ViewingPlatform;
+import javax.vecmath.Matrix3d;
+import javax.vecmath.Vector3d;
 
 import ab.j3d.Matrix3D;
 import ab.j3d.view.DragSupport;
@@ -45,133 +42,162 @@ public final class Java3dView
 	extends ViewModelView
 {
 	/**
-	 * Viewer with all Java 3D related information about this view.
-	 */
-	private final Viewer _viewer;
-
-	/**
 	 * Universe for which this view is defined.
 	 */
 	private final Java3dUniverse _universe;
 
 	/**
-	 * Construct Java3D view.
+	 * Transform group in view branch.
+	 *
+	 * @see     Java3dUniverse#createView
+	 */
+	private TransformGroup _tg;
+
+	/**
+	 * The <code>View</code> object is what ties all things together that are
+	 * needed to create a rendering of the scene.
+	 * <p />
+	 * Note that the <code>View</code> object is actually outside the scene
+	 * graph; it attaches to a <code>ViewPlatform</code> in the graph.
+	 *
+	 * @see     Java3dUniverse#createView
+	 */
+	private final View _view;
+
+	/**
+	 * Canvas with on-screen representation of this view.
+	 *
+	 * @see     Java3dUniverse#createView
+	 */
+	private Canvas3D _canvas;
+
+	/**
+	 * Cached <code>Matrix3d</code> instance (used by <code>update()</code).
+	 */
+	private Matrix3d _rotation = new Matrix3d();
+
+	/**
+	 * Cached <code>Vector3d</code> instance (used by <code>update()</code).
+	 */
+	private Vector3d _translation = new Vector3d();
+
+	/**
+	 * Cached <code>Matrix4d</code> instance (used by <code>update()</code).
+	 */
+	private Transform3D _transform3d = new Transform3D();
+
+	/**
+	 * Construct view node using Java3D for rendering.
 	 *
 	 * @param   universe        Java3D universe for which the view is created.
 	 * @param   id              Application-assigned ID of this view.
 	 * @param   viewControl     Control to use for this view.
+	 *
+	 * @see     Java3dUniverse#createView
 	 */
 	Java3dView( final Java3dUniverse universe , final Object id , final ViewControl viewControl )
 	{
 		super( id , viewControl );
 
-		final Locale locale = universe.getLocale();
+		/*
+		 * Create view branch.
+		 */
+		final TransformGroup tg     = new TransformGroup();
+		final Canvas3D       canvas = Java3dTools.createCanvas3D();
+		final View           view   = universe.createView( tg , canvas );
 
-		final Canvas3D canvas3d = Java3dTools.createCanvas3D();
-
-		final ViewingPlatform viewingPlatform = new ViewingPlatform( 1 );
-		viewingPlatform.setUniverse( universe );
-		//viewingPlatform.setViewPlatformBehavior( Java3dTools.createOrbitBehavior( canvas3d , universe.getUnit() ) );
-
-		final Viewer viewer = new Viewer( canvas3d );
-		viewer.setViewingPlatform( viewingPlatform );
-
-		_viewer = viewer;
-
-		// set transparency stuff
-		final View view = viewer.getView();
-		view.setDepthBufferFreezeTransparent( true );
-		view.setTransparencySortingPolicy( View.TRANSPARENCY_SORT_GEOMETRY );
-
-		view.setBackClipDistance( 100.0 );
-		view.setFrontClipDistance( 0.01 );
-		setProjectionPolicy( PERSPECTIVE );
-
-		// place view in universe
 		_universe = universe;
-		_universe.addViewer( viewer );
-		locale.addBranchGraph( viewingPlatform );
+		_tg       = tg;
+		_canvas   = canvas;
+		_view     = view;
 
-		// update view to initial transform
+		/*
+		 * Update view to initial transform.
+		 */
 		update();
 
-		// Add DragSupport to handle drag events.
-		final DragSupport ds = new DragSupport( canvas3d , universe.getUnit() );
+		/*
+		 * Add DragSupport to handle drag events.
+		 */
+		final DragSupport ds = new DragSupport( _canvas , universe.getUnit() );
 		ds.addDragListener( viewControl );
-	}
-
-	/**
-	 * Get canvas with on-screen representation of this view.
-	 *
-	 * @return  Canvas with on-screen representation of this view.
-	 */
-	private Canvas3D getCanvas3D()
-	{
-		return _viewer.getCanvas3D();
-	}
-
-	/**
-	 * Get transform group for this view.
-	 *
-	 * @return  <code>TransformGroup</code> for this view.
-	 */
-	private TransformGroup getTransformGroup()
-	{
-		final ViewingPlatform     vp  = _viewer.getViewingPlatform();
-		final MultiTransformGroup mtg = vp.getMultiTransformGroup();
-
-		return mtg.getTransformGroup( 0 );
-	}
-
-	/**
-	 * Get Java3D scene graph object.
-	 *
-	 * @return  Java3D scene graph object.
-	 */
-	public Group getSceneGraphObject()
-	{
-		return _viewer.getViewingPlatform();
 	}
 
 	public Component getComponent()
 	{
-		return getCanvas3D();
-	}
-
-	public void update()
-	{
-		final ViewControl    viewControl = getViewControl();
-		final TransformGroup tg          = getTransformGroup();
-
-		Matrix3D xform = viewControl.getTransform();
-
-		final double unit = _universe.getUnit();
-		if ( ( unit > 0 ) && ( unit != 1 ) )
-			xform = xform.setTranslation( xform.xo * unit , xform.yo * unit , xform.zo * unit );
-
-		tg.setTransform( Java3dTools.convertMatrix3DToTransform3D( xform.inverse() ) );
+		return _canvas;
 	}
 
 	public void setProjectionPolicy( final int policy )
 	{
+		final View view = _view;
+
 		switch ( policy )
 		{
 			case PERSPECTIVE :
-			{
-				final View view = _viewer.getView();
 				view.setProjectionPolicy( View.PERSPECTIVE_PROJECTION );
 				break;
-			}
 
 			case PARALLEL :
-			{
-				final View view = _viewer.getView();
 				view.setProjectionPolicy( View.PARALLEL_PROJECTION );
 				break;
-			}
 
 			default :
 				throw new IllegalArgumentException( "Invalid projection policy: " + policy );
 		}
+	}
+
+	public void update()
+	{
+		/*
+		 * Get the view-transform from the view control.
+		 */
+		final ViewControl control       = getViewControl();
+		final Matrix3D    viewTransform = control.getTransform();
+
+		/*
+		 * Determine rotation and translation. If a unit is set, use it to
+		 * scale the translation.
+		 */
+		final Java3dUniverse universe = _universe;
+		final double         unit     = universe.getUnit();
+
+		double xo = viewTransform.xo;
+		double yo = viewTransform.yo;
+		double zo = viewTransform.zo;
+
+		if ( ( unit > 0 ) && ( unit != 1 ) )
+		{
+			xo *= unit;
+			yo *= unit;
+			zo *= unit;
+		}
+
+		/*
+		 * Finally, set the 'TransformGroup' transform to the INVERSE matrix.
+		 *
+		 * Reuse 'Matrix3d', 'Vector3d', and 'Transform3D' objects here to
+		 * prevent creation of too many temporary objects
+		 * (sorry for the bad readability).
+		 */
+		final Matrix3d       rotation    = _rotation;
+		final Vector3d       translation = _translation;
+		final Transform3D    transform3d = _transform3d;
+		final TransformGroup tg          = _tg;
+
+		translation.x = - xo * ( rotation.m00 = viewTransform.xx )
+		                - yo * ( rotation.m01 = viewTransform.yx )
+		                - zo * ( rotation.m02 = viewTransform.zx );
+
+		translation.y = - xo * ( rotation.m10 = viewTransform.xy )
+		                - yo * ( rotation.m11 = viewTransform.yy )
+		                - zo * ( rotation.m12 = viewTransform.zy );
+
+		translation.z = - xo * ( rotation.m20 = viewTransform.xz )
+		                - yo * ( rotation.m21 = viewTransform.yz )
+		                - zo * ( rotation.m22 = viewTransform.zz );
+
+		transform3d.set( rotation , translation , 1.0 );
+		tg.setTransform( transform3d );
 	}
 }
