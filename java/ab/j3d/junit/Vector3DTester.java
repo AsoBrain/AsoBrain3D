aac7ec812c6cbdeee53dbf4032e36e7b800a2929
htl <htl>
2005-05-31 14:48:15 +0000

    Helper class to test a Vector3D object. ---------------------------------------------------------------------------- Create test to test the initial written 'sawsize' in a materialRequirement.

---
+++ java/ab/j3d/junit/Vector3DTester.java
@@ -0,0 +1,63 @@
+/* $Id$
+ * ====================================================================
+ * (C) Copyright Numdata BV 2005-2005
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * ====================================================================
+ */
+package ab.j3d.junit;
+
+import junit.framework.Assert;
+import junit.framework.AssertionFailedError;
+
+import ab.j3d.Vector3D;
+
+/**
+ * JUnit unit tool class to help with testing <code>Vector3D</code> objects.
+ *
+ * @see     Vector3D
+ *
+ * @author  H.B.J. te Lintelo
+ * @version $Revision$ $Date$
+ */
+public final class Vector3DTester
+	extends Assert
+{
+	/**
+	 * Utility class is not supposed to be instantiated.
+	 */
+	private Vector3DTester()
+	{
+	}
+
+	/**
+	 * Asserts that one vector is equal to another vector.
+	 *
+	 * @param   messagePrefix   Prefix to failure messages.
+	 * @param   expected        Expected vector value.
+	 * @param   actual          Actual vector value.
+	 * @param   delta           Delta value to limit the acceptable value range.
+	 *
+	 * @throws  AssertionFailedError is the assertion fails.
+	 */
+	public static void assertEquals( final String messagePrefix , final Vector3D expected , final Vector3D actual , final double delta )
+	{
+		final String actualPrefix = ( messagePrefix != null ) ? messagePrefix + " - " : "";
+
+		assertEquals( actualPrefix + "Incorrect 'x' value." , expected.x , actual.x , delta );
+		assertEquals( actualPrefix + "Incorrect 'y' value." , expected.y , actual.y , delta );
+		assertEquals( actualPrefix + "Incorrect 'z' value." , expected.z , actual.z , delta );
+	}
+}
