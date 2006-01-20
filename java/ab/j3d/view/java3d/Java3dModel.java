515b69cb336a830a956b2d0deeb0b00ca400fa83
ivenza <ivenza>
2006-01-20 15:07:51 +0000

    Removed method that used by an older version of selection support. Added automatic support of units

--- java/ab/j3d/view/java3d/Java3dModel.java
+++ java/ab/j3d/view/java3d/Java3dModel.java
@@ -1,6 +1,6 @@
 /* $Id$
  * ====================================================================
- * (C) Copyright Numdata BV 2004-2005
+ * (C) Copyright Numdata BV 2004-2006
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -24,12 +24,7 @@ import java.awt.Component;
 import java.util.HashMap;
 import java.util.Map;
 import javax.media.j3d.BranchGroup;
-import javax.media.j3d.Group;
-import javax.media.j3d.Node;
-import javax.media.j3d.Shape3D;
 import javax.media.j3d.TransformGroup;
-
-import com.sun.j3d.utils.picking.PickTool;
 
 import ab.j3d.Matrix3D;
 import ab.j3d.TextureSpec;
@@ -67,7 +62,8 @@ public final class Java3dModel
 	private final Map _nodeContentMap = new HashMap();
 
 	/**
-	 * Construct new Java 3D model.
+	 * Construct new Java 3D model using {@link ViewModel#MM} units
+	 * and a dark blueish background.
 	 */
 	public Java3dModel()
 	{
@@ -77,8 +73,8 @@ public final class Java3dModel
 	/**
 	 * Construct new Java 3D model.
 	 *
-	 * @param   unit            Unit scale factor (e.g. {@link ViewModel.MM}).
-	 * @param   background      Background color to use for 3D views.
+	 * @param   unit        Unit scale factor (e.g. {@link ViewModel.MM}).
+	 * @param   background  Background color to use for 3D views.
 	 */
 	public Java3dModel( final double unit , final Color background )
 	{
@@ -92,6 +88,8 @@ public final class Java3dModel
 	 */
 	public Java3dModel( final Java3dUniverse j3dUniverse )
 	{
+		super( j3dUniverse.getUnit() );
+
 		_universe     = j3dUniverse;
 		_contentGraph = Java3dTools.createDynamicScene( _universe.getContent() );
 	}
@@ -123,7 +121,6 @@ public final class Java3dModel
 		final BranchGroup nodeRoot = new BranchGroup();
 		nodeRoot.setCapability( BranchGroup.ALLOW_CHILDREN_READ );
 		nodeRoot.setCapability( BranchGroup.ALLOW_DETACH );
-		nodeRoot.setUserData( id );
 		_nodeContentMap.put( id , nodeRoot );
 
 		final TransformGroup nodeTransform = new TransformGroup();
@@ -131,7 +128,6 @@ public final class Java3dModel
 		nodeTransform.setCapability( TransformGroup.ALLOW_CHILDREN_WRITE );
 		nodeTransform.setCapability( TransformGroup.ALLOW_CHILDREN_EXTEND );
 		nodeTransform.setCapability( TransformGroup.ALLOW_TRANSFORM_WRITE );
-		nodeTransform.setUserData( nodeRoot );
 		nodeRoot.addChild( nodeTransform );
 
 		_contentGraph.addChild( nodeRoot );
@@ -160,10 +156,6 @@ public final class Java3dModel
 
 		final BranchGroup bg = Shape3DBuilder.createBranchGroup( nodes , textureOverride , opacity );
 
-		/* @FIXME: Because Java3D does not allow use of the getParent() method for a live scene, a node now has the parent in its user data. This will be fixed in j3d 1.4. */
-		updateChildren( bg );
-		bg.setUserData( nodeTransform );
-
 		/*
 		 * Attach content to scene graph (replace existing branch group).
 		 */
@@ -173,36 +165,6 @@ public final class Java3dModel
 			nodeTransform.addChild( bg );
 		else
 			nodeTransform.setChild( bg , 0 );
-	}
-
-	/**
-	 * Walks through the children of a group, and sets the parent as the user
-	 * object. This is necessary because Java3D 1.3 does not allow the use of
-	 * getParent() on a live scene.<br> If one of the children of
-	 * <code>group</code> is a Group itself, this method is called on that group as
-	 * well, so the tree is traversed recursively. If the child is a Shape3D, it it
-	 * set to allow intersection. This is required for picking an object.
-	 *
-	 * @param group The {@link Group} object for which the children must be
-	 *              updated.
-	 *
-	 */
-	private void updateChildren( final Group group )
-	{
-		for ( int i = 0; i < group.numChildren(); i++ )
-		{
-			final Node node = group.getChild( i );
-
-			node.setUserData( group );
-			if ( node instanceof Group )
-			{
-				updateChildren( (Group)node );
-			}
-			else if ( node instanceof Shape3D )
-			{
-				PickTool.setCapabilities( node, PickTool.INTERSECT_FULL );
-			}
-		}
 	}
 
 	public void removeNode( final Object id )
