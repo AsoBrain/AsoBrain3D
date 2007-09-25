16a0a10966276dfd08101c08875d1d21f18847e4
meinders <meinders>
2007-09-25 10:20:29 +0000

    Added support for a freely transformed box map.

--- java/ab/j3d/model/BoxUVMap.java
+++ java/ab/j3d/model/BoxUVMap.java
@@ -20,7 +20,10 @@
 package ab.j3d.model;
 
 import ab.j3d.Material;
+import ab.j3d.Matrix3D;
 import ab.j3d.Vector3D;
+
+import com.numdata.oss.MathTools;
 
 /**
  * Defines a box UV-mapping.
@@ -34,7 +37,12 @@ public class BoxUVMap
 	/**
 	 * UV map used to map the sides.
 	 */
-	private UVMap _sideMap;
+	private UVMap _leftMap;
+
+	/**
+	 * UV map used to map the sides.
+	 */
+	private UVMap _rightMap;
 
 	/**
 	 * UV map used to map the front and rear.
@@ -42,9 +50,34 @@ public class BoxUVMap
 	private UVMap _frontMap;
 
 	/**
+	 * UV map used to map the front and rear.
+	 */
+	private UVMap _backMap;
+
+	/**
 	 * UV map used to map the top and bottom.
 	 */
 	private UVMap _topMap;
+
+	/**
+	 * UV map used to map the top and bottom.
+	 */
+	private UVMap _bottomMap;
+
+	/**
+	 * Transformation applied to the box.
+	 */
+	private Matrix3D _transform;
+
+	/**
+	 * Construct new UV-map that applies a uniform box mapping.
+	 *
+	 * @param   modelUnits  Size of a model unit in meters.
+	 */
+	public BoxUVMap( final double modelUnits )
+	{
+		this( modelUnits , Vector3D.INIT , Matrix3D.INIT );
+	}
 
 	/**
 	 * Construct new UV-map that applies a uniform box mapping with the
@@ -55,9 +88,56 @@ public class BoxUVMap
 	 */
 	public BoxUVMap( final double modelUnits , final Vector3D origin )
 	{
-		_sideMap  = new PlanarUVMap( modelUnits , origin , Vector3D.INIT.set( 1.0 , 0.0 , 0.0 ) );
-		_frontMap = new PlanarUVMap( modelUnits , origin , Vector3D.INIT.set( 0.0 , 1.0 , 0.0 ) );
-		_topMap   = new PlanarUVMap( modelUnits , origin , Vector3D.INIT.set( 0.0 , 0.0 , 1.0 ) );
+		this( modelUnits , origin , Matrix3D.INIT );
+	}
+
+	/**
+	 * Construct new UV-map that applies a uniform box mapping with the
+	 * UV-origin located at the given spatial coordinates.
+	 *
+	 * @param   modelUnits  Size of a model unit in meters.
+	 * @param   origin      Location of the UV-origin in spatial coordinates.
+	 * @param   transform   Transformation applied to the box.
+	 */
+	public BoxUVMap( final double modelUnits , final Vector3D origin , final Matrix3D transform )
+	{
+		final Matrix3D translated = transform.setTranslation( origin );
+
+		final Matrix3D leftTransform   = translated.multiply( Matrix3D.INIT.set(
+			0.0 , -1.0 ,  0.0 , 0.0 ,
+			0.0 ,  0.0 ,  1.0 , 0.0 ,
+			0.0 ,  0.0 ,  0.0 , 0.0
+		) );
+		final Matrix3D rightTransform  = translated.multiply( Matrix3D.INIT.set(
+			0.0 , 1.0 , 0.0 , 0.0 ,
+			0.0 , 0.0 , 1.0 , 0.0 ,
+			0.0 , 0.0 , 0.0 , 0.0
+		) );
+		final Matrix3D frontTransform  = translated.multiply( Matrix3D.INIT.set(
+			1.0 , 0.0 , 0.0 , 0.0 ,
+			0.0 , 0.0 , 1.0 , 0.0 ,
+			0.0 , 0.0 , 0.0 , 0.0
+		) );
+		final Matrix3D backTransform   = translated.multiply( Matrix3D.INIT.set(
+			-1.0 , 0.0 , 0.0 , 0.0 ,
+			 0.0 , 0.0 , 1.0 , 0.0 ,
+			 0.0 , 0.0 , 0.0 , 0.0
+		) );
+		final Matrix3D topTransform    = translated.multiply( Matrix3D.INIT );
+		final Matrix3D bottomTransform = translated.multiply( Matrix3D.INIT.set(
+			1.0 ,  0.0 , 0.0 , 0.0 ,
+			0.0 , -1.0 , 0.0 , 0.0 ,
+			0.0 ,  0.0 , 0.0 , 0.0
+		) );
+
+		_leftMap   = new PlanarUVMap( modelUnits , leftTransform   );
+		_rightMap  = new PlanarUVMap( modelUnits , rightTransform  );
+		_frontMap  = new PlanarUVMap( modelUnits , frontTransform  );
+		_backMap   = new PlanarUVMap( modelUnits , backTransform   );
+		_topMap    = new PlanarUVMap( modelUnits , topTransform    );
+		_bottomMap = new PlanarUVMap( modelUnits , bottomTransform );
+
+		_transform = translated;
 	}
 
 	public void generate( final Material material , final double[] vertexCoordinates , final int[] vertexIndices , final float[] textureU , final float[] textureV )
@@ -76,29 +156,44 @@ public class BoxUVMap
 			final int base2 = vertexIndices[ 1 ] * 3;
 			final int base3 = vertexIndices[ 2 ] * 3;
 
-			final Vector3D normal = Vector3D.cross( vertexCoordinates[ base1     ] - vertexCoordinates[ base2     ] ,
-			                                        vertexCoordinates[ base1 + 1 ] - vertexCoordinates[ base2 + 1 ] ,
-			                                        vertexCoordinates[ base1 + 2 ] - vertexCoordinates[ base2 + 2 ] ,
-			                                        vertexCoordinates[ base3     ] - vertexCoordinates[ base2     ] ,
-			                                        vertexCoordinates[ base3 + 1 ] - vertexCoordinates[ base2 + 1 ] ,
-			                                        vertexCoordinates[ base3 + 2 ] - vertexCoordinates[ base2 + 2 ] );
+			Vector3D normal = Vector3D.cross( vertexCoordinates[ base1     ] - vertexCoordinates[ base2     ] ,
+			                                  vertexCoordinates[ base1 + 1 ] - vertexCoordinates[ base2 + 1 ] ,
+			                                  vertexCoordinates[ base1 + 2 ] - vertexCoordinates[ base2 + 2 ] ,
+			                                  vertexCoordinates[ base3     ] - vertexCoordinates[ base2     ] ,
+			                                  vertexCoordinates[ base3 + 1 ] - vertexCoordinates[ base2 + 1 ] ,
+			                                  vertexCoordinates[ base3 + 2 ] - vertexCoordinates[ base2 + 2 ] );
+
+			normal = _transform.inverseMultiply( normal );
+			normal = normal.normalize();
 
 			/*
 			 * Choose the appropriate planar map.
 			 */
-			final double threshold = 0.5 * Math.sqrt( 2.0 ) * normal.length();
+			final double threshold = 0.5 * Math.sqrt( 2.0 );
 
-			if ( Math.abs( normal.x ) >= threshold )
+			if ( MathTools.greaterOrAlmostEqual( normal.x , threshold ) )
 			{
-				_sideMap.generate( material , vertexCoordinates , vertexIndices , textureU , textureV );
+				_rightMap.generate( material , vertexCoordinates , vertexIndices , textureU , textureV );
 			}
-			else if ( Math.abs( normal.y ) >= threshold )
+			else if ( MathTools.lessOrAlmostEqual( normal.x , -threshold ) )
+			{
+				_leftMap.generate( material , vertexCoordinates , vertexIndices , textureU , textureV );
+			}
+			else if ( MathTools.greaterOrAlmostEqual( normal.y , threshold ) )
+			{
+				_backMap.generate( material , vertexCoordinates , vertexIndices , textureU , textureV );
+			}
+			else if ( MathTools.lessOrAlmostEqual( normal.y , -threshold ) )
 			{
 				_frontMap.generate( material , vertexCoordinates , vertexIndices , textureU , textureV );
 			}
-			else
+			else if ( MathTools.greaterOrAlmostEqual( normal.z , threshold ) )
 			{
 				_topMap.generate( material , vertexCoordinates , vertexIndices , textureU , textureV );
+			}
+			else
+			{
+				_bottomMap.generate( material , vertexCoordinates , vertexIndices , textureU , textureV );
 			}
 		}
 	}
