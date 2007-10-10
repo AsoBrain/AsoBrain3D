2052bde2f2cc546f3a9fb38c61b7dc679474a7f8
wijnand <wijnand>
2007-10-10 13:40:06 +0000

    ResourceLoader interface.

---
+++ java/ab/j3d/loader/ResourceLoader.java
@@ -0,0 +1,45 @@
+/* $Id$
+ * ====================================================================
+ * (C) Copyright Numdata BV 2004-2007
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * ====================================================================
+ */
+package ab.j3d.loader;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * This interface is used to retrieve resources from another resource, e.g. a
+ * zip archive.
+ *
+ * @author Wijnand Wieskamp
+ */
+public interface ResourceLoader
+{
+
+	/**
+	 * Loads a resource from the resource the loader was constructed with.
+	 *
+	 * @param name Name of the resource to be loaded
+	 *
+	 * @return {@link InputStream} containing the resource
+	 *
+	 * @throws IOException when resource could not be loaded.
+	 */
+	InputStream getResource( String name )
+		throws IOException;
+}
