46f7ab87aaf547c81bb4404f7fc7bb08066fe6e9
wijnand <wijnand>
2007-12-20 09:25:52 +0000

    Fixed JavaDoc.

--- java/ab/j3d/loader/ZipResourceLoader.java
+++ java/ab/j3d/loader/ZipResourceLoader.java
@@ -39,7 +39,8 @@ public class ZipResourceLoader
 	implements ResourceLoader
 {
 
-	/** The zip file as binary data constructed from {@link InputStream} @ constructor
+	/**
+	 *  The zip file as binary data constructed from {@link InputStream} @ constructor.
 	 */
 	private byte[] _binaryData = null;
 
