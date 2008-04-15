e5b700f021bd9cfd6a8ea4cb4f1248e42de338a3
heijnen <heijnen>
2008-04-15 13:55:12 +0000

    Implemented 'ManipulationMode' to make the control more generic. Will rename this class in the future.
    Added boolean result to 'mousePressed()' method of the 'PlaneControl' and 'SubPlaneControl' interface to allow controls to decline a request to start a drag operation.

--- java/ab/j3d/view/control/planar/PlaneMoveControl.java
+++ java/ab/j3d/view/control/planar/PlaneMoveControl.java
@@ -53,6 +53,21 @@ public class PlaneMoveControl
 	private Matrix3D _startTransform;
 
 	/**
+	 * Manipulation mode.
+	 */
+	public static enum ManipulationMode
+	{
+		/** Not manipulating anything. */ NONE ,
+		/** Move.                      */ MOVE ,
+		/** Rotate.                    */ ROTATE ,
+	}
+
+	/**
+	 * Current manipulation mode.
+	 */
+	private ManipulationMode _manipulationMode;
+
+	/**
 	 * Create control.
 	 *
 	 * @param   plane2wcs       Transformation from drag plane to WCS.
@@ -63,6 +78,7 @@ public class PlaneMoveControl
 		_plane2wcs = plane2wcs;
 		_twoSidedPlane = twoSidedPlane;
 		_startTransform = null;
+		_manipulationMode = ManipulationMode.NONE;
 	}
 
 	public Matrix3D getPlane2Wcs()
@@ -75,28 +91,87 @@ public class PlaneMoveControl
 		return _twoSidedPlane;
 	}
 
-	public void mousePressed( final ControlInputEvent event , final ViewModelNode viewModelNode , final Vector3D wcsStart )
+	/**
+	 * Get manipulation mode.
+	 *
+	 * @return  Manipulation mode.
+	 */
+	public ManipulationMode getManipulationMode()
 	{
-		super.mousePressed( event , viewModelNode , wcsStart );
+		return _manipulationMode;
+	}
+
+	public boolean mousePressed( final ControlInputEvent event , final ViewModelNode viewModelNode , final Vector3D wcsStart )
+	{
+		final ManipulationMode manipulationMode;
+
+		if ( super.mousePressed( event , viewModelNode , wcsStart ) )
+		{
+			manipulationMode = event.isControlDown() ? ManipulationMode.ROTATE : ManipulationMode.MOVE;
+		}
+		else
+		{
+			manipulationMode = ManipulationMode.NONE;
+		}
 
 		_startTransform = viewModelNode.getTransform();
+		_manipulationMode = manipulationMode;
+
+		return ( manipulationMode != ManipulationMode.NONE );
 	}
 
 	public void mouseDragged( final ControlInputEvent event , final ViewModelNode viewModelNode , final Vector3D wcsPoint )
 	{
 		super.mouseDragged( event , viewModelNode , wcsPoint );
 
-		viewModelNode.setTransform( _startTransform.plus( getWcsDelta() ) );
+		drag( viewModelNode , event );
 	}
 
 	public void mouseReleased( final ControlInputEvent event , final ViewModelNode viewModelNode , final Vector3D wcsPoint )
 	{
 		super.mouseReleased( event , viewModelNode , wcsPoint );
-
-		viewModelNode.setTransform( _startTransform.plus( getWcsDelta() ) );
+		drag( viewModelNode , event );
 	}
 
 	public void paint( final ViewModelView view , final Graphics2D g2d )
 	{
+	}
+
+	/**
+	 * Perform drag operation.
+	 *
+	 * @param   viewModelNode   Node that is being controlled.
+	 * @param   event           Event from control.
+	 */
+	protected void drag( final ViewModelNode viewModelNode , final ControlInputEvent event )
+	{
+		switch ( getManipulationMode() )
+		{
+			case MOVE :
+				viewModelNode.setTransform( _startTransform.setTranslation( getNodeEnd() ) );
+				break;
+
+			case ROTATE :
+				final double toDegrees         = Math.toDegrees( ViewModelView.DEFAULT_PIXELS_TO_RADIANS_FACTOR );
+				final double verticalDegrees   = -toDegrees * (double)event.getDragDeltaY();
+				final double horizontalDegrees =  toDegrees * (double)event.getDragDeltaX();
+
+				final Matrix3D rotation = Matrix3D.getTransform( 0.0 , 0.0 , verticalDegrees - horizontalDegrees , 0.0 , 0.0 , 0.0 );
+				viewModelNode.setTransform( rotation.multiply( _startTransform ) );
+				break;
+		}
+	}
+
+	/**
+	 * Get end point for node of drag operation in the world coordinate system (WCS).
+	 *
+	 * @return  End point for node of drag operation in WCS.
+	 */
+	protected Vector3D getNodeEnd()
+	{
+		final Matrix3D nodeTransform = _startTransform;
+		final Vector3D wcsDelta      = getWcsDelta();
+
+		return wcsDelta.plus( nodeTransform.xo , nodeTransform.yo , nodeTransform.zo );
 	}
 }
