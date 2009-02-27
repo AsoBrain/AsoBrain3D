eb299b7663df69d81c03fabb84fdf177fc0fa36a
meinders <meinders>
2009-02-27 12:10:35 +0000

    Added spot light support. Merged 'material' and 'specular' variables into a single 'result' variable. Renamed 'Iamb', 'Idiff' and 'Ispec' to 'ambient', 'diffuse' and 'specular'. Code maturity fixes.

--- java/ab/j3d/view/jogl/lighting-fragment.glsl
+++ java/ab/j3d/view/jogl/lighting-fragment.glsl
@@ -5,18 +5,25 @@ varying vec3 normal;
 
 /*
  * Per-pixel lighting.
+ *
+ * Based on source from: http://www.clockworkcoders.com/oglsl/tutorial5.htm
+ * See also: http://www.gamedev.net/reference/articles/article2428.asp
  */
 vec4 lighting( in vec4 color )
 {
-	vec3 material = vec3( 0.0 , 0.0 , 0.0 );
-	vec3 specular = vec3( 0.0 , 0.0 , 0.0 );
+	vec3 result = vec3( 0.0 , 0.0 , 0.0 );
 
-	// Based on source from: http://www.clockworkcoders.com/oglsl/tutorial5.htm
-	// And from: http://www.gamedev.net/reference/articles/article2428.asp
 	vec3 N = normalize( normal );
 	vec3 E = normalize( -vertex );
+
 	for ( int i = 0 ; i < lightCount ; i++ )
 	{
+		/*
+		 * Note on 'gl_LightSource' vs 'gl_FrontLightProduct':
+		 * For diffuse color, 'gl_FrontLightProduct.diffuse' is unsuitable,
+		 * because it includes the material's diffuse color, which is also
+		 * included in 'color' (multiplied with 'diffuse' below.)
+		 */
 		if ( gl_LightSource[ i ].position.w == 0.0 )
 		{
 			/*
@@ -24,41 +31,56 @@ vec4 lighting( in vec4 color )
 			 */
 			vec3 L = normalize( gl_LightSource[ i ].position.xyz );
 			vec3 H = gl_LightSource[ i ].halfVector.xyz;
-			vec3 Iamb  = gl_LightSource[ i ].ambient .rgb;
-			vec3 Idiff = gl_LightSource[ i ].diffuse .rgb *      max( dot( N , L ) , 0.0 );
-			vec3 Ispec = gl_LightSource[ i ].specular.rgb * pow( max( dot( N , H ) , 0.0 ) , gl_FrontMaterial.shininess );
+			vec3 ambient  = gl_FrontLightProduct[ i ].ambient .rgb;
+			vec3 diffuse  = gl_LightSource      [ i ].diffuse .rgb *      max( dot( N , L ) , 0.0 );
+			vec3 specular = gl_FrontLightProduct[ i ].specular.rgb * pow( max( dot( N , H ) , 0.0 ) , gl_FrontMaterial.shininess );
 
-			material += Iamb + color.rgb * Idiff;
-			specular += Ispec;
+			result += ambient + color.rgb * diffuse + specular;
 		}
 		else
 		{
-			/*
-			 * Point light.
-			 */
-			vec3 distVector = gl_LightSource[ i ].position.xyz - vertex;
-
-			vec3 L = normalize( distVector );
+			vec3 D = gl_LightSource[ i ].position.xyz - vertex;
+			vec3 L = normalize( D );
 			vec3 R = normalize( -reflect( L , N ) );
-			vec3 Iamb  = gl_FrontLightProduct[ i ].ambient .rgb;
-			vec3 Idiff = gl_LightSource      [ i ].diffuse .rgb *      max( dot( N , L ) , 0.0 );
-			vec3 Ispec = gl_FrontLightProduct[ i ].specular.rgb * pow( max( dot( R , E ) , 0.0 ) , gl_FrontMaterial.shininess );
 
-			/*
-			 * For diffuse color, gl_FrontLightProduct.diffuse is unsuitable,
-			 * because it includes the material's diffuse color, which is also
-			 * included in 'color' (multiplied with 'Idiff' below.)
-			 */
-
-			float dist = length( distVector );
+			float dist = length( D );
 			float attenuation = 1.0 / (
 				gl_LightSource[ i ].constantAttenuation  +
 				gl_LightSource[ i ].linearAttenuation    * dist +
 				gl_LightSource[ i ].quadraticAttenuation * dist * dist );
 
-			material += Iamb + color.rgb * Idiff * attenuation;
-			specular += Ispec * attenuation;
+			vec3 ambient = gl_FrontLightProduct[ i ].ambient.rgb;
+			vec3 diffuse;
+			vec3 specular;
+
+			if ( gl_LightSource[ i ].spotCutoff == 180.0 )
+			{
+				/*
+				 * Point light.
+				 */
+				diffuse  = gl_LightSource      [ i ].diffuse .rgb * attenuation *      max( dot( N , L ) , 0.0 );
+				specular = gl_FrontLightProduct[ i ].specular.rgb * attenuation * pow( max( dot( R , E ) , 0.0 ) , gl_FrontMaterial.shininess );
+			}
+			else
+			{
+				float spotEffect = dot( normalize( gl_LightSource[ i ].spotDirection ) , -L );
+				if ( spotEffect > gl_LightSource[ i ].spotCosCutoff )
+				{
+					attenuation *= pow( spotEffect , gl_LightSource[ i ].spotExponent );
+
+					diffuse   = gl_LightSource      [ i ].diffuse .rgb * attenuation *      max( dot( N , L ) , 0.0 );
+					specular  = gl_FrontLightProduct[ i ].specular.rgb * attenuation * pow( max( dot( R , E ) , 0.0 ) , gl_FrontMaterial.shininess );
+				}
+				else
+				{
+					diffuse   = vec3( 0.0 );
+					specular  = vec3( 0.0 );
+				}
+			}
+
+			result += ambient + color.rgb * diffuse + specular;
 		}
 	}
-	return vec4( gl_FrontLightModelProduct.sceneColor.rgb + material + specular , color.a );
+
+	return vec4( gl_FrontLightModelProduct.sceneColor.rgb + result , color.a );
 }
