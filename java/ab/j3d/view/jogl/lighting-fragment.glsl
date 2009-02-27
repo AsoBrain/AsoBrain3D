a24dfccd559ba7663d9c1313f6374a7e4adeb3ed
meinders <meinders>
2009-02-27 10:30:06 +0000

    Added directional light support.

--- java/ab/j3d/view/jogl/lighting-fragment.glsl
+++ java/ab/j3d/view/jogl/lighting-fragment.glsl
@@ -4,40 +4,61 @@ varying vec3 vertex;
 varying vec3 normal;
 
 /*
- * Per-pixel lighting. (point lights)
+ * Per-pixel lighting.
  */
 vec4 lighting( in vec4 color )
 {
 	vec3 material = vec3( 0.0 , 0.0 , 0.0 );
 	vec3 specular = vec3( 0.0 , 0.0 , 0.0 );
 
-	// based on source from: http://www.clockworkcoders.com/oglsl/tutorial5.htm
+	// Based on source from: http://www.clockworkcoders.com/oglsl/tutorial5.htm
+	// And from: http://www.gamedev.net/reference/articles/article2428.asp
 	vec3 N = normalize( normal );
 	vec3 E = normalize( -vertex );
 	for ( int i = 0 ; i < lightCount ; i++ )
 	{
-		vec3 distVector = gl_LightSource[ i ].position.xyz - vertex;
+		if ( gl_LightSource[ i ].position.w == 0.0 )
+		{
+			/*
+			 * Directional light.
+			 */
+			vec3 L = normalize( gl_LightSource[ i ].position.xyz );
+			vec3 H = gl_LightSource[ i ].halfVector.xyz;
+			vec3 Iamb  = gl_LightSource[ i ].ambient .rgb;
+			vec3 Idiff = gl_LightSource[ i ].diffuse .rgb *      max( dot( N , L ) , 0.0 );
+			vec3 Ispec = gl_LightSource[ i ].specular.rgb * pow( max( dot( N , H ) , 0.0 ) , gl_FrontMaterial.shininess );
 
-		vec3 L = normalize( distVector );
-		vec3 R = normalize( -reflect( L , N ) );
-		vec3 Iamb  = gl_FrontLightProduct[ i ].ambient .rgb;
-		vec3 Idiff = gl_LightSource      [ i ].diffuse .rgb *      max( dot( N , L ) , 0.0 );
-		vec3 Ispec = gl_FrontLightProduct[ i ].specular.rgb * pow( max( dot( R , E ) , 0.0 ) , 0.3 * gl_FrontMaterial.shininess );
+			material += Iamb + color.rgb * Idiff;
+			specular += Ispec;
+		}
+		else
+		{
+			/*
+			 * Point light.
+			 */
+			vec3 distVector = gl_LightSource[ i ].position.xyz - vertex;
 
-		/*
-		 * For diffuse color, gl_FrontLightProduct.diffuse is unsuitable,
-		 * because it includes the material's diffuse color, which is also
-		 * included in 'color' (multiplied with 'Idiff' below.)
-		 */
+			vec3 L = normalize( distVector );
+			vec3 R = normalize( -reflect( L , N ) );
+			vec3 Iamb  = gl_FrontLightProduct[ i ].ambient .rgb;
+			vec3 Idiff = gl_LightSource      [ i ].diffuse .rgb *      max( dot( N , L ) , 0.0 );
+			vec3 Ispec = gl_FrontLightProduct[ i ].specular.rgb * pow( max( dot( R , E ) , 0.0 ) , gl_FrontMaterial.shininess );
 
-		float dist = length( distVector );
-		float attenuation = 1.0 / (
-			gl_LightSource[ i ].constantAttenuation  +
-			gl_LightSource[ i ].linearAttenuation    * dist +
-			gl_LightSource[ i ].quadraticAttenuation * dist * dist );
+			/*
+			 * For diffuse color, gl_FrontLightProduct.diffuse is unsuitable,
+			 * because it includes the material's diffuse color, which is also
+			 * included in 'color' (multiplied with 'Idiff' below.)
+			 */
 
-		material += Iamb + color.rgb * Idiff * attenuation;
-		specular += Ispec * attenuation;
+			float dist = length( distVector );
+			float attenuation = 1.0 / (
+				gl_LightSource[ i ].constantAttenuation  +
+				gl_LightSource[ i ].linearAttenuation    * dist +
+				gl_LightSource[ i ].quadraticAttenuation * dist * dist );
+
+			material += Iamb + color.rgb * Idiff * attenuation;
+			specular += Ispec * attenuation;
+		}
 	}
 	return vec4( gl_FrontLightModelProduct.sceneColor.rgb + material + specular , color.a );
 }
