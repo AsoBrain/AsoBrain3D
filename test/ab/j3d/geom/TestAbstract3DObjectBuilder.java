fe97472c3446f6cfd3db1776df1597f20b6861f8
heijnen <heijnen>
2009-07-27 13:07:44 +0000

    Added 'getShapeClass()' method and 'ShapeClass' enumeration to 'classify' shapes.

---
+++ test/ab/j3d/geom/TestAbstract3DObjectBuilder.java
@@ -0,0 +1,113 @@
+/* $Id$
+ * ====================================================================
+ * AsoBrain 3D Toolkit
+ * Copyright (C) 1999-2009 Peter S. Heijnen
+ * Copyright (C) 2009-2009 Numdata BV
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * ====================================================================
+ */
+package ab.j3d.geom;
+
+import java.awt.Shape;
+import java.awt.geom.Arc2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+
+import junit.framework.TestCase;
+
+import ab.j3d.geom.Abstract3DObjectBuilder.ShapeClass;
+import static ab.j3d.geom.Abstract3DObjectBuilder.getShapeClass;
+
+/**
+ * This class tests the {@link Abstract3DObjectBuilder} class.
+ *
+ * @author  Peter S. Heijnen
+ * @version $Revision$ ($Date$, $Author$)
+ */
+public final class TestAbstract3DObjectBuilder
+	extends TestCase
+{
+	/**
+	 * Name of this class.
+	 */
+	private static final String CLASS_NAME = TestAbstract3DObjectBuilder.class.getName();
+
+	/**
+	 * Test {@link Abstract3DObjectBuilder#getShapeClass} method.
+	 */
+	public static void testGetShapeClass()
+	{
+		System.out.println( CLASS_NAME + ".testGetShapeClass" );
+
+		/**
+		 * Defines a test.
+		 *
+		 * @noinspection JavaDoc
+		 */
+		class Test
+		{
+			final Shape _shape;
+			final ShapeClass _expectedNormal;
+			final ShapeClass _expectedFlattened;
+
+			Test( final ShapeClass expectedNormal , final ShapeClass expectedFlattened , final Shape shape )
+			{
+				_shape = shape;
+				_expectedNormal = expectedNormal;
+				_expectedFlattened = expectedFlattened;
+			}
+		}
+
+		/*
+		 * Tests to execute
+		 */
+		final Test[] tests =
+		{
+			new Test( ShapeClass.OPEN_PATH      , ShapeClass.OPEN_PATH    , new Arc2D.Double( 20.0 , 10.0 , 100.0 , 80.0 ,  45.0 , -90.0 , Arc2D.OPEN ) ) ,
+			new Test( ShapeClass.CURVED_COMPLEX , ShapeClass.CCW_CONVEX   , new Arc2D.Double( 20.0 , 10.0 , 100.0 , 80.0 ,  45.0 , -90.0 , Arc2D.PIE ) ) ,
+			new Test( ShapeClass.CURVED_COMPLEX , ShapeClass.CCW_CONVEX   , new Arc2D.Double( 20.0 , 10.0 , 100.0 , 80.0 ,  45.0 , -90.0 , Arc2D.CHORD ) ) ,
+			new Test( ShapeClass.OPEN_PATH      , ShapeClass.OPEN_PATH    , new Arc2D.Double( 20.0 , 10.0 , 100.0 , 80.0 , -45.0 ,  90.0 , Arc2D.OPEN ) ) ,
+			new Test( ShapeClass.CURVED_COMPLEX , ShapeClass.CW_CONVEX    , new Arc2D.Double( 20.0 , 10.0 , 100.0 , 80.0 , -45.0 ,  90.0 , Arc2D.PIE ) ) ,
+			new Test( ShapeClass.CURVED_COMPLEX , ShapeClass.CW_CONVEX    , new Arc2D.Double( 20.0 , 10.0 , 100.0 , 80.0 , -45.0 ,  90.0 , Arc2D.CHORD ) ) ,
+			new Test( ShapeClass.VOID           , ShapeClass.VOID         , new Arc2D.Double( 20.0 , 10.0 ,   0.0 ,  0.0 ,  45.0 , -90.0 , Arc2D.PIE ) ) ,
+			new Test( ShapeClass.VOID           , ShapeClass.VOID         , new Arc2D.Double( 20.0 , 10.0 , -10.0 , -1.0 ,  45.0 , -90.0 , Arc2D.PIE ) ) ,
+			new Test( ShapeClass.CCW_CONVEX     , ShapeClass.CCW_CONVEX   , new Arc2D.Double( 20.0 , 10.0 , 100.0 , 80.0 ,   0.0 ,   0.0 , Arc2D.PIE ) ) ,
+			new Test( ShapeClass.CURVED_COMPLEX , ShapeClass.CCW_CONVEX   , new Ellipse2D.Double( 20.0 , 10.0 , 100.0 , 80.0 ) ) ,
+			new Test( ShapeClass.VOID           , ShapeClass.VOID         , new Ellipse2D.Double( 20.0 , 10.0 , 0.0 , 0.0 ) ) ,
+			new Test( ShapeClass.VOID           , ShapeClass.VOID         , new Ellipse2D.Double( 20.0 , 10.0 , -100.0 , 0.0 ) ) ,
+			new Test( ShapeClass.CCW_QUAD       , ShapeClass.CCW_QUAD     , new Rectangle2D.Double( 20.0 , 10.0 , 100.0 ,  80.0 ) ) ,
+			new Test( ShapeClass.VOID           , ShapeClass.VOID         , new Rectangle2D.Double( 20.0 , 10.0 , 0.0 , 0.0 ) ) ,
+			new Test( ShapeClass.VOID           , ShapeClass.VOID         , new Rectangle2D.Double( 20.0 , 10.0 , 0.0 , -80.0 ) ) ,
+			new Test( ShapeClass.LINE_SEGMENT   , ShapeClass.LINE_SEGMENT , new Line2D.Double( 20.0 , 10.0 , 100.0 , 80.0 ) ) ,
+			new Test( ShapeClass.VOID           , ShapeClass.VOID         , new Line2D.Double( 20.0 , 10.0 , 20.0 , 10.0 ) ) ,
+		};
+
+		/*
+		 * Execute tests.
+		 */
+		for ( int i = 0 ; i < tests.length ; i++ )
+		{
+			System.out.println( " - Test #" + ( i + 1 ) );
+
+			final Test test = tests[ i ];
+			assertEquals( "Unexpected 'getShapeClass( shape.getPathIterator( null ) )' result" , test._expectedNormal , getShapeClass( test._shape.getPathIterator( null ) ) );
+			assertEquals( "Unexpected 'getShapeClass( shape.getPathIterator( null , 0.5 ) )' result" , test._expectedFlattened ,  getShapeClass( test._shape.getPathIterator( null , 0.5 ) ) );
+			assertEquals( "Unexpected 'getShapeClass( shape , 0.0 )' result" , test._expectedNormal , getShapeClass( test._shape , 0.0 ) );
+			assertEquals( "Unexpected 'getShapeClass( shape , 0.5 )' result" , test._expectedFlattened , getShapeClass( test._shape , 0.5 ) );
+		}
+	}
+}
