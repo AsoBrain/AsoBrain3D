2efff1baffa5cf2f719a1b25a8c4efa12e8f64db
meinders <meinders>
2010-02-26 14:15:35 +0000

    Added support for cube maps.

--- java/ab/j3d/view/jogl/TextureCache.java
+++ java/ab/j3d/view/jogl/TextureCache.java
@@ -1,6 +1,6 @@
 /* $Id$
  * ====================================================================
- * (C) Copyright Numdata BV 2009-2009
+ * (C) Copyright Numdata BV 2009-2010
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -19,6 +19,8 @@
  */
 package ab.j3d.view.jogl;
 
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.util.HashMap;
@@ -29,6 +31,7 @@ import javax.media.opengl.GL;
 import javax.media.opengl.GLException;
 
 import com.sun.opengl.util.texture.Texture;
+import com.sun.opengl.util.texture.TextureData;
 import com.sun.opengl.util.texture.TextureIO;
 
 import ab.j3d.MapTools;
@@ -61,6 +64,42 @@ public class TextureCache
 	 * Set of textures, by name, with an alpha channel.
 	 */
 	private final Set<String> _alpha;
+
+	/**
+	 * Specifies the orientation/layout of a cube map represented as a single
+	 * texture. Constants are named after the side that the center square faces.
+	 */
+	private enum CubeMapOrientation
+	{
+		/**
+		 * Rotated 90 degrees around X-axis, compared to {@link #POSITIVE_Z}.
+		 * <pre>
+		 *     +---+
+		 *     | Z+|
+		 * +---+---+---+---+
+		 * | X-| Y-| X+| Y+|
+		 * +---+---+---+---+
+		 *     | Z-|
+		 *     +---+
+		 * </pre>
+		 */
+		NEGATIVE_Y ,
+
+		/**
+		 * Default orientation for OpenGL, meaning that no rotation of
+		 * sub-images is required using this layout.
+		 * <pre>
+		 *     +---+
+		 *     | Y+|
+		 * +---+---+---+---+
+		 * | X-| Z+| X+| Z-|
+		 * +---+---+---+---+
+		 *     | Y-|
+		 *     +---+
+		 * </pre>
+		 */
+		POSITIVE_Z
+	}
 
 	/**
 	 * Construct new texture cache.
@@ -322,6 +361,208 @@ public class TextureCache
 			result = JOGLTools.createNormalizationCubeMap( gl );
 			_textures.put( NORMALIZATION_CUBE_MAP , result );
 		}
+		return result;
+	}
+
+	/**
+	 * Returns a cube map based on the specified image. The image must have
+	 * an aspect ratio of 4:3, consisting of 12 squares with the following
+	 * layout:
+	 * <pre>
+	 *     +---+
+	 *     | Y+|
+	 * +---+---+---+---+
+	 * | X-| Z+| X+| Z-|
+	 * +---+---+---+---+
+	 *     | Y-|
+	 *     +---+
+	 * </pre>
+	 *
+	 * @param   gl      OpenGL pipeline.
+	 * @param   cube    Name of the cube map image.
+	 *
+	 * @return  Cube map texture.
+	 */
+	public Texture getCubeMap( final GL gl , final String cube )
+	{
+		final String key = "cube:" + cube;
+		Texture result = _textures.get( key );
+
+		if ( result == null )
+		{
+			final BufferedImage cubeImage = MapTools.loadImage( cube );
+			if ( cubeImage != null )
+			{
+				if ( cubeImage.getWidth() * 3 != cubeImage.getHeight() * 4 )
+				{
+					throw new IllegalArgumentException( "Cube map must have 4:3 aspect ratio" );
+				}
+
+				if ( cubeImage.getWidth() % 4 != 0 )
+				{
+					throw new IllegalArgumentException( "Cube map width must be a multiple of 4" );
+				}
+
+				if ( cubeImage.getHeight() % 3 != 0 )
+				{
+					throw new IllegalArgumentException( "Cube map height must be a multiple of 3" );
+				}
+
+				result = TextureIO.newTexture( GL.GL_TEXTURE_CUBE_MAP );
+				result.bind();
+
+				/*
+				 * Create the six sides of the cube map.
+				 */
+				loadCubeMap( gl , cubeImage , CubeMapOrientation.POSITIVE_Z );
+
+				gl.glTexParameteri( GL.GL_TEXTURE_CUBE_MAP , GL.GL_TEXTURE_MIN_FILTER , GL.GL_LINEAR        );
+				gl.glTexParameteri( GL.GL_TEXTURE_CUBE_MAP , GL.GL_TEXTURE_MAG_FILTER , GL.GL_LINEAR        );
+				gl.glTexParameteri( GL.GL_TEXTURE_CUBE_MAP , GL.GL_TEXTURE_WRAP_S     , GL.GL_CLAMP_TO_EDGE );
+				gl.glTexParameteri( GL.GL_TEXTURE_CUBE_MAP , GL.GL_TEXTURE_WRAP_T     , GL.GL_CLAMP_TO_EDGE );
+				gl.glTexParameteri( GL.GL_TEXTURE_CUBE_MAP , GL.GL_TEXTURE_WRAP_R     , GL.GL_CLAMP_TO_EDGE );
+
+				_textures.put( key, result );
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * Returns a cube map based on the specified images.
+	 *
+	 * @param   gl  OpenGL pipeline.
+	 * @param   x1  Image on the negative-X side of the cube.
+	 * @param   y1  Image on the negative-Y side of the cube.
+	 * @param   z1  Image on the negative-Z side of the cube.
+	 * @param   x2  Image on the positive-X side of the cube.
+	 * @param   y2  Image on the positive-Y side of the cube.
+	 * @param   z2  Image on the positive-Z side of the cube.
+	 *
+	 * @return  Cube map texture.
+	 */
+	public Texture getCubeMap( final GL gl , final String x1 , final String y1 , final String z1 , final String x2 , final String y2 , final String z2 )
+	{
+		final String key = "cube6:" + x1 + ":" + y1 + ":" + z1 + ":" + x2 + ":" + y2 + ":" + z2;
+		Texture result = _textures.get( key );
+
+		if ( result == null )
+		{
+			result = TextureIO.newTexture( GL.GL_TEXTURE_CUBE_MAP );
+			result.bind();
+
+			/*
+			 * Create the six sides of the cube map.
+			 */
+			loadCubeMap( gl , GL.GL_TEXTURE_CUBE_MAP_NEGATIVE_X , x1 );
+			loadCubeMap( gl , GL.GL_TEXTURE_CUBE_MAP_NEGATIVE_Y , y1 );
+			loadCubeMap( gl , GL.GL_TEXTURE_CUBE_MAP_NEGATIVE_Z , z1 );
+			loadCubeMap( gl , GL.GL_TEXTURE_CUBE_MAP_POSITIVE_X , x2 );
+			loadCubeMap( gl , GL.GL_TEXTURE_CUBE_MAP_POSITIVE_Y , y2 );
+			loadCubeMap( gl , GL.GL_TEXTURE_CUBE_MAP_POSITIVE_Z , z2 );
+
+			gl.glTexParameteri( GL.GL_TEXTURE_CUBE_MAP , GL.GL_TEXTURE_MIN_FILTER , GL.GL_LINEAR        );
+			gl.glTexParameteri( GL.GL_TEXTURE_CUBE_MAP , GL.GL_TEXTURE_MAG_FILTER , GL.GL_LINEAR        );
+			gl.glTexParameteri( GL.GL_TEXTURE_CUBE_MAP , GL.GL_TEXTURE_WRAP_S     , GL.GL_CLAMP_TO_EDGE );
+			gl.glTexParameteri( GL.GL_TEXTURE_CUBE_MAP , GL.GL_TEXTURE_WRAP_T     , GL.GL_CLAMP_TO_EDGE );
+			gl.glTexParameteri( GL.GL_TEXTURE_CUBE_MAP , GL.GL_TEXTURE_WRAP_R     , GL.GL_CLAMP_TO_EDGE );
+
+			_textures.put( key, result );
+		}
+
+		return result;
+	}
+
+	/**
+	 * Loads the given image as a cube map into the texture that is bound to the
+	 * active texture unit.
+	 *
+	 * @param   gl              OpenGL pipeline.
+	 * @param   image           Image to be loaded.
+	 * @param   orientation     Orientation of the cube map representation.
+	 */
+	private static void loadCubeMap( final GL gl , final BufferedImage image , final CubeMapOrientation orientation )
+	{
+		final int size = image.getWidth() / 4;
+		switch ( orientation )
+		{
+			case POSITIVE_Z:
+				loadCubeMap( gl , GL.GL_TEXTURE_CUBE_MAP_NEGATIVE_X , getSubImage( image , 0 , 0 , size , size , size ) );
+				loadCubeMap( gl , GL.GL_TEXTURE_CUBE_MAP_NEGATIVE_Y , getSubImage( image , 0 , size , 2 * size , size , size ) );
+				loadCubeMap( gl , GL.GL_TEXTURE_CUBE_MAP_NEGATIVE_Z , getSubImage( image , 0 , 3 * size , size , size , size ) );
+				loadCubeMap( gl , GL.GL_TEXTURE_CUBE_MAP_POSITIVE_X , getSubImage( image , 0 , 2 * size , size , size , size ) );
+				loadCubeMap( gl , GL.GL_TEXTURE_CUBE_MAP_POSITIVE_Y , getSubImage( image , 0 , size , 0 , size , size ) );
+				loadCubeMap( gl , GL.GL_TEXTURE_CUBE_MAP_POSITIVE_Z , getSubImage( image , 0 , size , size , size , size ) );
+				break;
+
+			case NEGATIVE_Y:
+				loadCubeMap( gl , GL.GL_TEXTURE_CUBE_MAP_NEGATIVE_X , getSubImage( image , -90 , 0 , size , size , size ) );
+				loadCubeMap( gl , GL.GL_TEXTURE_CUBE_MAP_NEGATIVE_Y , getSubImage( image , 0 , size , size , size , size ) );
+				loadCubeMap( gl , GL.GL_TEXTURE_CUBE_MAP_NEGATIVE_Z , getSubImage( image , 0 , size , 2 * size , size , size ) );
+				loadCubeMap( gl , GL.GL_TEXTURE_CUBE_MAP_POSITIVE_X , getSubImage( image , 90 , 2 * size , size , size , size ) );
+				loadCubeMap( gl , GL.GL_TEXTURE_CUBE_MAP_POSITIVE_Y , getSubImage( image , 180 , 3 * size , size , size , size ) );
+				loadCubeMap( gl , GL.GL_TEXTURE_CUBE_MAP_POSITIVE_Z , getSubImage( image , 0 , size , 0 , size , size ) );
+				break;
+		}
+	}
+
+	/**
+	 * Loads the specified image as a cube map into the texture that is bound to
+	 * the active texture unit.
+	 *
+	 * @param   gl      OpenGL pipeline.
+	 * @param   target  Texture target.
+	 * @param   image   Image to be loaded.
+	 */
+	private static void loadCubeMap( final GL gl , final int target , final String image )
+	{
+		final BufferedImage bufferedImage = MapTools.loadImage( image );
+		if ( bufferedImage != null )
+		{
+			final BufferedImage compatibleImage = createCompatibleTextureImage( bufferedImage , gl );
+			loadCubeMap( gl , target , compatibleImage );
+		}
+	}
+
+	/**
+	 * Loads the given image as a cube map into the texture that is bound to the
+	 * active texture unit.
+	 *
+	 * @param   gl      OpenGL pipeline.
+	 * @param   target  Texture target.
+	 * @param   image   Image to be loaded.
+	 */
+	private static void loadCubeMap( final GL gl , final int target , final BufferedImage image )
+	{
+		final TextureData textureData = TextureIO.newTextureData( image , GL.GL_RGB8 , GL.GL_RGB , false );
+		gl.glTexImage2D( target , 0 , textureData.getInternalFormat() , textureData.getWidth() , textureData.getHeight() , textureData.getBorder() , textureData.getPixelFormat() , textureData.getPixelType() , textureData.getBuffer() );
+		textureData.flush();
+	}
+
+	/**
+	 * Returns a sub-image from the given image.
+	 *
+	 * @param   image   Source image.
+	 * @param   angle   Angle of rotation, in degrees.
+	 * @param   x       Top-left corner x-coordinate of the source rectangle.
+	 * @param   y       Top-left corner y-coordinate of the source rectangle.
+	 * @param   w       Width of the source rectangle.
+	 * @param   h       Height of the source rectangle.
+	 *
+	 * @return  Specified sub-image.
+	 */
+	private static BufferedImage getSubImage( final BufferedImage image , final int angle , final int x , final int y , final int w , final int h )
+	{
+		final BufferedImage result = new BufferedImage( w , h , image.getType() );
+		final Graphics2D g = result.createGraphics();
+		final AffineTransform transform = new AffineTransform();
+		transform.translate( (double)( w / 2 ) , (double)( h / 2 ) );
+		transform.rotate( Math.toRadians( (double)-angle ) );
+		transform.translate( (double)( -x - w / 2 ) , (double)( -y - h / 2 ) );
+		g.setTransform( transform );
+		g.drawImage( image , 0 , 0 , null );
+		g.dispose();
 		return result;
 	}
 }
