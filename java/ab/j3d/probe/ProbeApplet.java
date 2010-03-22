2d0dc06c09e2333e322dc2b0d4908fb06b1bf125
meinders <meinders>
2010-03-22 09:32:36 +0000

    Create probe UI on EDT. Destroy UI when applet is destroyed.

--- java/ab/j3d/probe/ProbeApplet.java
+++ java/ab/j3d/probe/ProbeApplet.java
@@ -1,7 +1,7 @@
 /*
  * $Id$
  *
- * (C) Copyright Numdata BV 2009-2009 - All Rights Reserved
+ * (C) Copyright Numdata BV 2009-2010 - All Rights Reserved
  *
  * This software may not be used, copied, modified, or distributed in any
  * form without express permission from Numdata BV. Please contact Numdata BV
@@ -10,7 +10,9 @@
 package ab.j3d.probe;
 
 import java.awt.BorderLayout;
+import java.lang.reflect.InvocationTargetException;
 import javax.swing.JApplet;
+import javax.swing.SwingUtilities;
 
 /**
  * Runs the 3D capabilities probe as an applet.
@@ -21,6 +23,9 @@ import javax.swing.JApplet;
 public class ProbeApplet
 	extends JApplet
 {
+	/**
+	 * User interface component.
+	 */
 	private ProbeUI _ui;
 
 	/**
@@ -28,16 +33,35 @@ public class ProbeApplet
 	 */
 	public ProbeApplet()
 	{
+		_ui = null;
 	}
 
 	@Override
 	public void init()
 	{
-		final ProbeUI ui = new ProbeUI();
-		_ui = ui;
+		try
+		{
+			SwingUtilities.invokeAndWait( new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					final ProbeUI ui = new ProbeUI();
+					_ui = ui;
 
-		setLayout( new BorderLayout() );
-		add( ui , BorderLayout.CENTER );
+					setLayout( new BorderLayout() );
+					add( ui , BorderLayout.CENTER );
+				}
+			} );
+		}
+		catch ( InterruptedException e )
+		{
+			e.printStackTrace();
+		}
+		catch ( InvocationTargetException e )
+		{
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -53,5 +77,11 @@ public class ProbeApplet
 	@Override
 	public void destroy()
 	{
+		final ProbeUI ui = _ui;
+		if ( ui != null )
+		{
+			ui.dispose();
+			_ui = null;
+		}
 	}
 }
