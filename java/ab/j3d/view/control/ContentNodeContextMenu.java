e08191b48f1dd82354b88603b97792b70613eede
meinders <meinders>
2010-05-20 13:12:25 +0000

    Made constructor public.

--- java/ab/j3d/view/control/ContentNodeContextMenu.java
+++ java/ab/j3d/view/control/ContentNodeContextMenu.java
@@ -19,11 +19,10 @@
  */
 package ab.j3d.view.control;
 
-import javax.swing.JPopupMenu;
+import javax.swing.*;
 
-import ab.j3d.control.ControlInputEvent;
-import ab.j3d.model.ContentNode;
-import ab.j3d.model.Face3DIntersection;
+import ab.j3d.control.*;
+import ab.j3d.model.*;
 
 /**
  * This class implements a context menu for a {@link ContentNode}.
@@ -58,7 +57,7 @@ public class ContentNodeContextMenu
 	 * @param   contentNode         {@link ContentNode} for which this popup is created.
 	 * @param   face3DIntersection  {@link Face3DIntersection} between node and pointer.
 	 */
-	ContentNodeContextMenu( final String label , final ControlInputEvent controlInputEvent , final ContentNode contentNode, final Face3DIntersection face3DIntersection )
+	public ContentNodeContextMenu( final String label , final ControlInputEvent controlInputEvent , final ContentNode contentNode, final Face3DIntersection face3DIntersection )
 	{
 		super( label );
 		_contentNode = contentNode;
