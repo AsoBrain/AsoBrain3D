3a927adda847d0ddaf596d7fbc30fe0c12e63660
heijnen <heijnen>
2010-06-01 19:09:28 +0000

    Modified 'Triangulation' interface and added new 'BasicTriangulation' implementation class to support primitives (triangle sets, triangle strips, triangle fans). Do not flip normal direction of tesselator.

--- java/ab/j3d/geom/GLUTriangulator.java
+++ java/ab/j3d/geom/GLUTriangulator.java
@@ -19,22 +19,16 @@
  */
 package ab.j3d.geom;
 
-import java.awt.Shape;
-import java.awt.geom.PathIterator;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.awt.*;
+import java.awt.geom.*;
+import java.util.*;
+import javax.media.opengl.glu.*;
+
+import ab.j3d.*;
+import ab.j3d.geom.Triangulation.*;
+import ab.j3d.geom.Triangulation.Primitive.*;
 import static javax.media.opengl.GL.*;
-import javax.media.opengl.glu.GLU;
 import static javax.media.opengl.glu.GLU.*;
-import javax.media.opengl.glu.GLUtessellator;
-import javax.media.opengl.glu.GLUtessellatorCallbackAdapter;
-
-import ab.j3d.Matrix3D;
-import ab.j3d.Vector3D;
-
-import com.numdata.oss.HashList;
 
 /**
  * Implements the {@link Triangulator} interface using the tesselator provided
@@ -62,29 +56,34 @@ class GLUTriangulator
 	GLUTriangulator()
 	{
 		_flatness = 1.0;
-		_normal   = Vector3D.INIT;
+		_normal = Vector3D.POSITIVE_Z_AXIS;
 	}
 
+	@Override
 	public double getFlatness()
 	{
 		return _flatness;
 	}
 
+	@Override
 	public void setFlatness( final double flatness )
 	{
 		_flatness = flatness;
 	}
 
+	@Override
 	public Vector3D getNormal()
 	{
 		return _normal;
 	}
 
+	@Override
 	public void setNormal( final Vector3D normal )
 	{
 		_normal = normal;
 	}
 
+	@Override
 	public Triangulation triangulate( final Shape shape )
 	{
 		final GLU glu = new GLU();
@@ -95,11 +94,11 @@ class GLUTriangulator
 		switch ( iterator.getWindingRule() )
 		{
 			case PathIterator.WIND_EVEN_ODD:
-				glu.gluTessProperty( tessellator , GLU_TESS_WINDING_RULE , (double)GLU_TESS_WINDING_ODD );
+				glu.gluTessProperty( tessellator, GLU_TESS_WINDING_RULE, (double)GLU_TESS_WINDING_ODD );
 				break;
 
 			case PathIterator.WIND_NON_ZERO:
-				glu.gluTessProperty( tessellator , GLU_TESS_WINDING_RULE , (double)GLU_TESS_WINDING_NONZERO );
+				glu.gluTessProperty( tessellator, GLU_TESS_WINDING_RULE, (double)GLU_TESS_WINDING_NONZERO );
 				break;
 
 			default:
@@ -109,30 +108,31 @@ class GLUTriangulator
 		final Vector3D normal = _normal;
 		if ( normal != Vector3D.INIT )
 		{
-			glu.gluTessNormal( tessellator , normal.x , normal.y , normal.z );
+			glu.gluTessNormal( tessellator, normal.x, normal.y, normal.z );
 		}
 
-		final TriangulationImpl    triangulation        = new TriangulationImpl();
+		final BasicTriangulation triangulation = new BasicTriangulation();
 		final TriangulationBuilder triangulationBuilder = new TriangulationBuilder( triangulation );
 
-		glu.gluTessCallback( tessellator , GLU_TESS_BEGIN   , triangulationBuilder );
-		glu.gluTessCallback( tessellator , GLU_TESS_VERTEX  , triangulationBuilder );
-		glu.gluTessCallback( tessellator , GLU_TESS_END     , triangulationBuilder );
-		glu.gluTessCallback( tessellator , GLU_TESS_ERROR   , triangulationBuilder );
-		glu.gluTessCallback( tessellator , GLU_TESS_COMBINE , triangulationBuilder );
+		glu.gluTessCallback( tessellator, GLU_TESS_BEGIN  , triangulationBuilder );
+		glu.gluTessCallback( tessellator, GLU_TESS_VERTEX , triangulationBuilder );
+		glu.gluTessCallback( tessellator, GLU_TESS_END    , triangulationBuilder );
+		glu.gluTessCallback( tessellator, GLU_TESS_ERROR  , triangulationBuilder );
+		glu.gluTessCallback( tessellator, GLU_TESS_COMBINE, triangulationBuilder );
 
 		glu.gluBeginPolygon( tessellator );
 
-		createContour( triangulation, glu , tessellator , false , iterator );
+		createContour( triangulation, glu, tessellator, false, iterator );
 
 		glu.gluEndPolygon( tessellator );
 
 		return triangulationBuilder.getTriangulation();
 	}
 
-	public Triangulation triangulate( final Shape positive , final Iterable<? extends Shape> negative )
+	@Override
+	public Triangulation triangulate( final Shape positive, final Iterable<? extends Shape> negative )
 	{
-		final TriangulationImpl    triangulation        = new TriangulationImpl();
+		final BasicTriangulation triangulation = new BasicTriangulation();
 		final TriangulationBuilder triangulationBuilder = new TriangulationBuilder( triangulation );
 
 		final Vector3D normal = _normal;
@@ -144,22 +144,22 @@ class GLUTriangulator
 		final GLU glu = new GLU();
 
 		final GLUtessellator tessellator = glu.gluNewTess();
-		glu.gluTessProperty( tessellator , GLU_TESS_WINDING_RULE , (double)GLU_TESS_WINDING_ODD );
-		glu.gluTessCallback( tessellator , GLU_TESS_BEGIN   , triangulationBuilder );
-		glu.gluTessCallback( tessellator , GLU_TESS_VERTEX  , triangulationBuilder );
-		glu.gluTessCallback( tessellator , GLU_TESS_END     , triangulationBuilder );
-		glu.gluTessCallback( tessellator , GLU_TESS_ERROR   , triangulationBuilder );
-		glu.gluTessCallback( tessellator , GLU_TESS_COMBINE , triangulationBuilder );
+		glu.gluTessProperty( tessellator, GLU_TESS_WINDING_RULE, (double)GLU_TESS_WINDING_ODD );
+		glu.gluTessCallback( tessellator, GLU_TESS_BEGIN  , triangulationBuilder );
+		glu.gluTessCallback( tessellator, GLU_TESS_VERTEX , triangulationBuilder );
+		glu.gluTessCallback( tessellator, GLU_TESS_END    , triangulationBuilder );
+		glu.gluTessCallback( tessellator, GLU_TESS_ERROR  , triangulationBuilder );
+		glu.gluTessCallback( tessellator, GLU_TESS_COMBINE, triangulationBuilder );
 
 		glu.gluBeginPolygon( tessellator );
 
-		glu.gluTessNormal( tessellator , -normal.x , -normal.y , -normal.z );
-		boolean contourStarted = createContour( triangulation , glu , tessellator , false , positive.getPathIterator( null , _flatness ) );
+		glu.gluTessNormal( tessellator, normal.x, normal.y, normal.z );
+		boolean contourStarted = createContour( triangulation, glu, tessellator, false, positive.getPathIterator( null, _flatness ) );
 
-		glu.gluTessNormal( tessellator , normal.x , normal.y , normal.z );
+		glu.gluTessNormal( tessellator, -normal.x, -normal.y, -normal.z );
 		for ( final Shape shape : negative )
 		{
-			contourStarted = createContour( triangulation , glu , tessellator , contourStarted , shape.getPathIterator( null , _flatness ) );
+			contourStarted = createContour( triangulation, glu, tessellator, contourStarted, shape.getPathIterator( null, _flatness ) );
 		}
 
 		glu.gluEndPolygon( tessellator );
@@ -180,7 +180,7 @@ class GLUTriangulator
 	 * @return  <code>true</code> if a contour was started;
 	 *          <code>false</code> if no contour was started.
 	 */
-	private static boolean createContour( final TriangulationImpl triangulation , final GLU glu , final GLUtessellator tessellator , final boolean contourWasStarted , final PathIterator pathIterator )
+	private static boolean createContour( final BasicTriangulation triangulation, final GLU glu, final GLUtessellator tessellator, final boolean contourWasStarted, final PathIterator pathIterator )
 	{
 		final double[] coords = new double[ 6 ];
 		final double[] moveCoords = new double[ 6 ];
@@ -215,16 +215,16 @@ class GLUTriangulator
 							glu.gluTessBeginContour( tessellator );
 						}
 
-						final int vertexIndex = triangulation.addVertex( Vector3D.INIT.set( moveCoords[ 0 ] , moveCoords[ 1 ] , 0.0 ) );
-						glu.gluTessVertex( tessellator , moveCoords , 0 , Integer.valueOf( vertexIndex ) );
+						final int vertexIndex = triangulation.addVertex( Vector3D.INIT.set( moveCoords[ 0 ], moveCoords[ 1 ], 0.0 ) );
+						glu.gluTessVertex( tessellator, moveCoords, 0, Integer.valueOf( vertexIndex ) );
 
 						contourStarted = true;
 						moved = false;
 					}
 
-					final int vertexIndex = triangulation.addVertex( Vector3D.INIT.set( coords[ 0 ] , coords[ 1 ] , 0.0 ) );
+					final int vertexIndex = triangulation.addVertex( Vector3D.INIT.set( coords[ 0 ], coords[ 1 ], 0.0 ) );
 					coords[ 2 ] = 0.0;
-					glu.gluTessVertex( tessellator , coords , 0 , Integer.valueOf( vertexIndex ) );
+					glu.gluTessVertex( tessellator, coords, 0, Integer.valueOf( vertexIndex ) );
 					break;
 				}
 
@@ -238,9 +238,8 @@ class GLUTriangulator
 		return contourStarted;
 	}
 
-
 	/**
-	 * Builds the list of triangles for a {@link TriangulationImpl} from the
+	 * Builds the list of triangles for a {@link BasicTriangulation} from the
 	 * callbacks it receives from a GLU tessellator.
 	 */
 	private static class TriangulationBuilder
@@ -249,26 +248,23 @@ class GLUTriangulator
 		/**
 		 * Triangulation being built.
 		 */
-		private final TriangulationImpl _triangulation;
-
-		/**
-		 * Stores up to two vertex indices gathered from previous events.
-		 * Elements that don't contain a vertex index are set to '-1'.
-		 *
-		 * @see     #addToVertexBuffer(int)
-		 */
-		private final int[] _vertexBuffer;
+		private final BasicTriangulation _triangulation;
 
 		/**
 		 * Specifies the type of primitive represented by vertices presented
 		 * between calls to {@link #begin(int)} and {@link #end()}.
 		 */
-		private int _type;
+		private Type _type;
 
 		/**
-		 * Indicates whether triangles should be flipped.
+		 * Stores vertices of the primitive that is currently being built.
 		 */
-		private boolean _flip;
+		private int[] _vertexBuffer;
+
+		/**
+		 * Current number of vertices in {@link #_vertexBuffer}.
+		 */
+		private int _vertexCount;
 
 		/**
 		 * Constructs a new triangulation builder that operates on the given
@@ -276,12 +272,12 @@ class GLUTriangulator
 		 *
 		 * @param   triangulation   Triangulation being built.
 		 */
-		TriangulationBuilder( final TriangulationImpl triangulation )
+		TriangulationBuilder( final BasicTriangulation triangulation )
 		{
 			_triangulation = triangulation;
-			_vertexBuffer  = new int[ 2 ];
-			_type          = -1;
-			_flip          = false;
+			_type = Type.TRIANGLES;
+			_vertexBuffer = new int[ 8 ];
+			_vertexCount = 0;
 		}
 
 		/**
@@ -294,67 +290,63 @@ class GLUTriangulator
 			return _triangulation;
 		}
 
-		public void begin( final int type )
+		@Override
+		public void begin( final int glType )
 		{
-			_type = type;
-			_flip = false;
+			final Type type;
 
-			final int[] vertexBuffer = _vertexBuffer;
-			vertexBuffer[ 0 ] = -1;
-			vertexBuffer[ 1 ] = -1;
+			switch ( glType )
+			{
+				case GL_TRIANGLES:
+					type = Type.TRIANGLES;
+					break;
+
+				case GL_TRIANGLE_STRIP:
+					type = Type.TRIANGLE_STRIP;
+					break;
+
+				case GL_TRIANGLE_FAN:
+					type = Type.TRIANGLE_FAN;
+					break;
+
+				default:
+					throw new AssertionError( "Unexpected type: " + _type );
+			}
+
+			_type = type;
+			_vertexCount = 0;
 		}
 
+		@Override
 		public void vertex( final Object data )
 		{
-			final int vertexIndex = (Integer)data;
+			int[] vertexBuffer = _vertexBuffer;
+			final int vertexCount = _vertexCount;
 
-			if ( !addToVertexBuffer( vertexIndex ) )
+			if ( vertexCount == vertexBuffer.length )
 			{
-				final int[] triangle = getTriangle( vertexIndex );
-				if ( isTriangle( triangle ) )
-				{
-					_triangulation.addTriangle( triangle );
-				}
-
-				switch ( _type )
-				{
-					case GL_TRIANGLE_FAN:
-						_vertexBuffer[ 1 ] = vertexIndex;
-						break;
-
-					case GL_TRIANGLE_STRIP:
-						_vertexBuffer[ 0 ] = _vertexBuffer[ 1 ];
-						_vertexBuffer[ 1 ] = vertexIndex;
-						_flip = !_flip;
-						break;
-
-					case GL_TRIANGLES:
-						_vertexBuffer[ 0 ] = -1;
-						_vertexBuffer[ 1 ] = -1;
-						break;
-
-					default:
-						throw new AssertionError( "Unexpected type: " + _type );
-				}
+				vertexBuffer = Arrays.copyOf( vertexBuffer, vertexCount * 2 );
+				_vertexBuffer = vertexBuffer;
 			}
-		}
 
-		public void combine( final double[] coords , final Object[] vertexData , final float[] weight , final Object[] output )
+			vertexBuffer[ vertexCount ] = (Integer)data;
+			_vertexCount = vertexCount + 1;
+		}
+		
+		@Override
+		public void combine( final double[] coords, final Object[] vertexData, final float[] weight, final Object[] output )
 		{
-			if (                                  !vertexData[ 0 ].equals( vertexData[ 1 ] )   ||
-			     ( ( vertexData[ 1 ] != null ) && !vertexData[ 0 ].equals( vertexData[ 1 ] ) ) ||
-			     ( ( vertexData[ 2 ] != null ) && !vertexData[ 0 ].equals( vertexData[ 2 ] ) ) )
-			{
-				final int combinedIndex = _triangulation.addVertex( Vector3D.INIT.set( coords[ 0 ], coords[ 1 ], coords[ 2 ] ) );
-				output[ 0 ] = Integer.valueOf( combinedIndex );
-			}
+			output[ 0 ] = Integer.valueOf( _triangulation.addVertex( new Vector3D( coords[ 0 ], coords[ 1 ], coords[ 2 ] ) ) );
 		}
 
+		@Override
 		public void end()
 		{
-			_type = -1;
+			_triangulation.addPrimitive( new Primitive( _type, Arrays.copyOf( _vertexBuffer, _vertexCount ) ) );
+			_vertexCount = 0;
 		}
 
+		@Override
 		public void error( final int errorCode )
 		{
 			final GLU glu = new GLU();
@@ -370,158 +362,6 @@ class GLUTriangulator
 			}
 
 			throw new RuntimeException( errorString );
-		}
-
-		/**
-		 * Returns the triangle formed by the two vertices in the vertex buffer
-		 * and the given vertex.
-		 *
-		 * @param   vertexIndex     Vertex index of the third vertex of the
-		 *                          triangle.
-		 * @return  Vertex indices of the corners of the triangle.
-		 */
-		private int[] getTriangle( final int vertexIndex )
-		{
-			final int[] vertexBuffer = _vertexBuffer;
-			return _flip ? new int[] { vertexBuffer[ 0 ] , vertexBuffer[ 1 ] , vertexIndex }
-			             : new int[] { vertexBuffer[ 1 ] , vertexBuffer[ 0 ] , vertexIndex };
-		}
-
-		/**
-		 * Returns whether the given vertex triplet forms a triangle, as opposed
-		 * to a line or point.
-		 *
-		 * @param   triplet     Vertex indices to be evaluated.
-		 *
-		 * @return  <code>true</code> if the vertices represent a triangle;
-		 *          <code>false</code> otherwise.
-		 */
-		private boolean isTriangle( final int[] triplet )
-		{
-			final List<Vector3D> vertices = _triangulation._vertices;
-			final Vector3D vertex0 = vertices.get( triplet[ 0 ] );
-			final Vector3D vertex1 = vertices.get( triplet[ 1 ] );
-			final Vector3D vertex2 = vertices.get( triplet[ 2 ] );
-
-			final Vector3D edge01 = vertex1.minus( vertex0 );
-			final Vector3D edge02 = vertex2.minus( vertex0 );
-
-			// Two edges of a triangle can't be co-linear.
-			return !Vector3D.INIT.equals( Vector3D.cross( edge01 , edge02 ) );
-		}
-
-		/**
-		 * Adds the given vertex index to the vertex buffer if it's not full.
-		 *
-		 * @param   vertexIndex     Vertex index to be added.
-		 *
-		 * @return  <code>true</code> if the vertex index was added;
-		 *          <code>false</code> if the buffer was full.
-		 */
-		private boolean addToVertexBuffer( final int vertexIndex )
-		{
-			final boolean result;
-
-			final int[] vertexBuffer = _vertexBuffer;
-
-			if ( vertexBuffer[ 1 ] == -1 )
-			{
-				if ( vertexBuffer[ 0 ] == -1 )
-				{
-					vertexBuffer[ 0 ] = vertexIndex;
-					result = true;
-				}
-				else
-				{
-					vertexBuffer[ 1 ] = vertexIndex;
-					result = true;
-				}
-			}
-			else
-			{
-				result = false;
-			}
-
-			return result;
-		}
-	}
-
-	/**
-	 * Basic implementation of a triangulation result.
-	 */
-	private static class TriangulationImpl implements Triangulation
-	{
-		/**
-		 * Vertex indices of the triangles that the triangulation consists of.
-		 */
-		private final List<int[]> _triangles;
-
-		/**
-		 * Vertex coordinates used in the triangulation.
-		 */
-		private final List<Vector3D> _vertices;
-
-		/**
-		 * Constructs a new triangulation.
-		 */
-		TriangulationImpl()
-		{
-			_triangles = new ArrayList<int[]>();
-			_vertices  = new HashList<Vector3D>();
-		}
-
-		/**
-		 * Adds the given vertex to the triangulation, returning its index.
-		 *
-		 * @param   vertex  Vertex to be added.
-		 *
-		 * @return  Vertex index.
-		 */
-		public int addVertex( final Vector3D vertex )
-		{
-			int result = _vertices.indexOf( vertex );
-			if ( result == -1 )
-			{
-				_vertices.add( vertex );
-				result = _vertices.size() - 1;
-			}
-			return result;
-		}
-
-		/**
-		 * Adds the given triangle to the triangulation.
-		 *
-		 * @param   triangle    Triangle to be added, specified as three vertex
-		 *                      indices (see {@link #getTriangles()}).
-		 */
-		public void addTriangle( final int[] triangle )
-		{
-			_triangles.add( triangle );
-		}
-
-		public Collection<int[]> getTriangles()
-		{
-			return Collections.unmodifiableList( _triangles );
-		}
-
-		public List<Vector3D> getVertices( final Matrix3D transform )
-		{
-			final List<Vector3D> result;
-
-			if ( transform.equals( Matrix3D.INIT ) )
-			{
-				result = _vertices;
-			}
-			else
-			{
-				result = new ArrayList<Vector3D>( _vertices.size() );
-				for ( final Vector3D vertex : _vertices )
-				{
-					result.add( transform.transform( vertex ) );
-				}
-			}
-
-			return result;
 		}
 	}
 }
