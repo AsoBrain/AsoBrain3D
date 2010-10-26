13d90944b6a769b8262d54ef875bceba3cfc4d79
meinders <meinders>
2010-10-26 14:58:31 +0000

    Added change listener support.

--- java/ab/j3d/view/FrameCounter.java
+++ java/ab/j3d/view/FrameCounter.java
@@ -1,15 +1,28 @@
-/*
- * $Id$
+/* $Id$
+ * ====================================================================
+ * AsoBrain 3D Toolkit
+ * Copyright (C) 1999-2010 Peter S. Heijnen
  *
- * (C) Copyright Numdata BV 2009-2009 - All Rights Reserved
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This software may not be used, copied, modified, or distributed in any
- * form without express permission from Numdata BV. Please contact Numdata BV
- * for license information.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * ====================================================================
  */
 package ab.j3d.view;
 
-import java.util.concurrent.TimeUnit;
+import java.util.*;
+import java.util.concurrent.*;
+import javax.swing.event.*;
 
 /**
  * Provides a frame count per second.
@@ -38,11 +51,17 @@ public class FrameCounter
 	private int _value = 0;
 
 	/**
+	 * Listeners to be notified when the frame counter value is updated.
+	 */
+	private final List<ChangeListener> _listeners;
+
+	/**
 	 * Constructs a new frame counter.
 	 */
 	public FrameCounter()
 	{
 		_start = System.nanoTime();
+		_listeners = new ArrayList<ChangeListener>();
 	}
 
 	/**
@@ -77,6 +96,43 @@ public class FrameCounter
 			_start += TimeUnit.SECONDS.toNanos( (long)secondsPassed );
 			_value = _counter / secondsPassed;
 			_counter = 0;
+			fireChangeEvent();
+		}
+	}
+
+	/**
+	 * Adds a listener. The listener will be notified when the frame counter
+	 * value is updated.
+	 *
+	 * @param   changeListener  Change listener to be added.
+	 */
+	public void addChangeListener( final ChangeListener changeListener )
+	{
+		_listeners.add( changeListener );
+	}
+
+	/**
+	 * Removes a listener.
+	 *
+	 * @param   changeListener  Change listener to be added.
+	 */
+	public void removeChangeListener( final ChangeListener changeListener )
+	{
+		_listeners.remove( changeListener );
+	}
+
+	/**
+	 * Notifies all listeners.
+	 */
+	protected void fireChangeEvent()
+	{
+		if ( !_listeners.isEmpty() )
+		{
+			final ChangeEvent event = new ChangeEvent( this );
+			for ( final ChangeListener listener : _listeners )
+			{
+				listener.stateChanged( event );
+			}
 		}
 	}
 }
