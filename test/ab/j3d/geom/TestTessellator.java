020d61a32dc3944532d38b2b79fad9b45e54c1df
heijnen <heijnen>
2010-12-19 22:07:31 +0000

    Large-scale refactoring of tessellator code in AsoBrain 3D Toolkit:  - Reasoning:     > Currently sides of shapes with holes in them are not properly extruded. The normals of inner bounary edges are flipped.     > Since the code borrowed from the OpenGL sample implementation is always available, we no longer need the 'AreaTessellator'.     > The same calculations are done multiple times for a single extruded shape and a lot of intermediate objects are created. We should be able to find a more direct approach with fewer intermediate objects.  - Implementation:     > Rigorously refactored the OpenGL sample implementation. Some big changes:        - The 'Mesh' class is now the central API of the tessellation algorithm (the 'GLUtessellator' interface class was removed).        - Tried to make code a bit more Java-specific and introduced a little bit of object oriented code (everything was static, a lot still is).        - A single tessellation result stored in a 'Mesh' can be used to create multiple sets of output, like counter-clockwise primitives, clockwise primitives, and outlines (either counter-clockwise or clockwise). This especially useful to create extrusions.     > Simplified 'Tessellation' model and removed 'TessellationBuilder' interface. Vertex coordinates are no longer stored in the 'Tessellation' itself, but in a simple list of 2D points. This decouples the 3D projection of the tessellated shape, allowing reuse of the vertex list and not requiring a 'TessellationBuilder' as we did before. We removed the 'TessellationBuilder' altogether and use simple collections instead for vertices aswell as primitives and outlines.     > The 'Tessellator' interface was replaced by a new 'Tessellator' class based on the beforementioned 'Mesh' class. The most notable change is that the tessellation process was split into a shape defintion and tessellation result extraction. This allows the user to use only those features that are actually required in a more flexible way.     > Modified 'Abstract3DObjectBuilder.addExtrudedShape()' to make used of the new tessellation features.

---
+++ test/ab/j3d/geom/TestTessellator.java
@@ -0,0 +1,162 @@
+/* $Id$
+ * ====================================================================
+ * AsoBrain 3D Toolkit
+ * Copyright (C) 1999-2010 Peter S. Heijnen
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * ====================================================================
+ */
+package ab.j3d.geom;
+
+import java.awt.*;
+import java.awt.geom.*;
+import java.util.*;
+
+import com.numdata.oss.*;
+import junit.framework.*;
+
+/**
+ * Unit test for the {@link Tessellator}.
+ *
+ * @author  G. Meinders
+ * @version $Revision$ $Date$
+ */
+public class TestTessellator
+	extends TestCase
+{
+	/**
+	 * Test some extra shapes to test basic tessellator functionality.
+	 *
+	 * @throws  Exception if the test fails.
+	 */
+	public static void testExtremeShapes()
+		throws Exception
+	{
+		{
+			final Shape emptyPath = new Path2D.Float();
+
+			final Tessellator tessellator = new Tessellator();
+			tessellator.defineShape( emptyPath, 0.0 );
+
+			final HashList<Point2D> vertexList = new HashList<Point2D>();
+			final Collection<TessellationPrimitive> primitives = tessellator.constructPrimitives( vertexList, true );
+			tessellator.constructOutlines( vertexList, true );
+
+			Assert.assertTrue( "Expected no primitives." , primitives.isEmpty() );
+		}
+
+		{
+			final Path2D point = new Path2D.Float();
+			point.moveTo( 10.0 , 10.0 );
+
+			final Tessellator tessellator = new Tessellator();
+			tessellator.defineShape( point, 0.0 );
+
+			final HashList<Point2D> vertexList = new HashList<Point2D>();
+			final Collection<TessellationPrimitive> primitives = tessellator.constructPrimitives( vertexList, true );
+			tessellator.constructOutlines( vertexList, true );
+
+			Assert.assertTrue( "Expected no primitives." , primitives.isEmpty() );
+		}
+
+		{
+			final Path2D point = new Path2D.Float();
+			point.moveTo( 10.0 , 10.0 );
+			point.closePath();
+
+			final Tessellator tessellator = new Tessellator();
+			tessellator.defineShape( point, 0.0 );
+
+			final HashList<Point2D> vertexList = new HashList<Point2D>();
+			final Collection<TessellationPrimitive> primitives = tessellator.constructPrimitives( vertexList, true );
+			tessellator.constructOutlines( vertexList, true );
+
+			Assert.assertTrue( "Expected no primitives." , primitives.isEmpty() );
+		}
+
+		{
+			final Path2D line = new Path2D.Float();
+			line.moveTo( 10.0 , 10.0 );
+			line.lineTo( 20.0 , 30.0 );
+
+			final Tessellator tessellator = new Tessellator();
+			tessellator.defineShape( line, 0.0 );
+
+			final HashList<Point2D> vertexList = new HashList<Point2D>();
+			final Collection<TessellationPrimitive> primitives = tessellator.constructPrimitives( vertexList, true );
+			tessellator.constructOutlines( vertexList, true );
+
+			Assert.assertTrue( "Expected no primitives." , primitives.isEmpty() );
+		}
+
+		{
+			final Path2D clockwise = new Path2D.Float();
+			clockwise.moveTo( 10.0 , 10.0 );
+			clockwise.lineTo( 20.0 , 30.0 );
+			clockwise.lineTo( 15.0 , 5.0 );
+			clockwise.closePath();
+
+			final Tessellator tessellator = new Tessellator();
+			tessellator.defineShape( clockwise, 0.0 );
+
+			final HashList<Point2D> vertexList = new HashList<Point2D>();
+			final Collection<TessellationPrimitive> primitives = tessellator.constructPrimitives( vertexList, true );
+			tessellator.constructOutlines( vertexList, true );
+
+			Assert.assertFalse( "Expected primitives." , primitives.isEmpty() );
+		}
+
+		{
+			final Path2D clockwise = new Path2D.Float();
+			clockwise.moveTo( 10.0 , 10.0 );
+			clockwise.lineTo( 20.0 , 30.0 );
+			clockwise.lineTo( 15.0 , 5.0 );
+			clockwise.closePath();
+			clockwise.moveTo( 110.0 , 10.0 );
+			clockwise.lineTo( 120.0 , 30.0 );
+			clockwise.lineTo( 115.0 , 5.0 );
+			clockwise.closePath();
+
+			final Tessellator tessellator = new Tessellator();
+			tessellator.defineShape( clockwise, 0.0 );
+
+			final HashList<Point2D> vertexList = new HashList<Point2D>();
+			final Collection<TessellationPrimitive> primitives = tessellator.constructPrimitives( vertexList, true );
+			tessellator.constructOutlines( vertexList, true );
+
+			Assert.assertFalse( "Expected primitives." , primitives.isEmpty() );
+		}
+
+		{
+			final Path2D clockwise = new Path2D.Float();
+			clockwise.moveTo( 10.0 , 10.0 );
+			clockwise.lineTo( 20.0 , 30.0 );
+			clockwise.lineTo( 15.0 , 5.0 );
+			clockwise.closePath();
+			clockwise.moveTo( 110.0 , 10.0 );
+			clockwise.lineTo( 120.0 , 30.0 );
+			clockwise.lineTo( 115.0 , 5.0 );
+
+			final Tessellator tessellator = new Tessellator();
+			tessellator.defineShape( clockwise, 0.0 );
+
+			final HashList<Point2D> vertexList = new HashList<Point2D>();
+			final Collection<TessellationPrimitive> primitives = tessellator.constructPrimitives( vertexList, true );
+			tessellator.constructOutlines( vertexList, true );
+
+			Assert.assertFalse( "Expected primitives." , primitives.isEmpty() );
+		}
+	}
+}
