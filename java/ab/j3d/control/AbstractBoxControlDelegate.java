7510f8a02b0c196fd0835c4a2a9ac43a42a32de1
meinders <meinders>
2011-06-27 09:48:47 +0000

    Issue 2275: Controls for controlling objects using their bounding boxes.  - Simplify implementation of delegates with this abstract no-op implementation.

---
+++ java/ab/j3d/control/AbstractBoxControlDelegate.java
@@ -0,0 +1,62 @@
+/*
+ * $Id$
+ * ====================================================================
+ * AsoBrain 3D Toolkit
+ * Copyright (C) 1999-2011 Peter S. Heijnen
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * ====================================================================
+ */
+package ab.j3d.control;
+
+import ab.j3d.*;
+import ab.j3d.model.*;
+
+/**
+ * Empty implementation of the {@link BoxControlDelegate} interface.
+ *
+ * @author G. Meinders
+ * @version $Revision$ $Date$
+ */
+public class AbstractBoxControlDelegate
+	implements BoxControlDelegate
+{
+	@Override
+	public boolean isEnabled()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean isVisible()
+	{
+		return isEnabled();
+	}
+
+	@Override
+	public void mousePressed( final ControlInputEvent event, final ContentNode node )
+	{
+	}
+
+	@Override
+	public void mouseDragged( final ControlInputEvent event, final ContentNode node, final Vector3D offset )
+	{
+	}
+
+	@Override
+	public void mouseReleased( final ControlInputEvent event, final ContentNode node )
+	{
+	}
+}
