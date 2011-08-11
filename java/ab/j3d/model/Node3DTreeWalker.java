4f4ba5dba04da6c42a9e0a89eca6cbb054f21852
meinders <meinders>
2011-08-11 11:51:31 +0000

    Issue 2316: Allow for the tree walker to be instantiated, such that (part of) its behavior can be overridden.

--- java/ab/j3d/model/Node3DTreeWalker.java
+++ java/ab/j3d/model/Node3DTreeWalker.java
@@ -49,7 +49,8 @@ public class Node3DTreeWalker
 	 */
 	public static boolean walk( @NotNull final Node3DVisitor visitor, @NotNull final Node3D node )
 	{
-		return walk( visitor, Matrix3D.IDENTITY, node );
+		final Node3DTreeWalker treeWalker = new Node3DTreeWalker();
+		return treeWalker.walkNode( visitor, Matrix3D.IDENTITY, node );
 	}
 
 	/**
@@ -68,7 +69,8 @@ public class Node3DTreeWalker
 	 */
 	public static boolean walk( @NotNull final Node3DVisitor visitor, @NotNull final Matrix3D transform, @NotNull final Node3D node )
 	{
-		return walk( visitor, createPath( null, transform, node ) );
+		final Node3DTreeWalker treeWalker = new Node3DTreeWalker();
+		return treeWalker.walkNode( visitor, transform, node );
 	}
 
 	/**
@@ -85,6 +87,68 @@ public class Node3DTreeWalker
 	 */
 	public static boolean walk( @NotNull final Node3DVisitor visitor, @NotNull final Node3DPath path )
 	{
+		final Node3DTreeWalker treeWalker = new Node3DTreeWalker();
+		return treeWalker.walkNode( visitor, path );
+	}
+
+	/**
+	 * Constructs a new instance.
+	 */
+	public Node3DTreeWalker()
+	{
+	}
+
+	/**
+	 * Perform depth-first scene graph walk, starting at the given node with an
+	 * identity transformation.
+	 * <p>
+	 * The <code>visitor</code> is called for each visited node, including the
+	 * given <code>node</code>.
+	 *
+	 * @param   visitor     Visitor that will be called for each visited node.
+	 * @param   node        Root {@link Node3D} to start tree walk at.
+	 *
+	 * @return  <code>true</code> if the tree walk was finished normally;
+	 *          <code>false</code> if the tree walk was aborted.
+	 */
+	public boolean walkNode( @NotNull final Node3DVisitor visitor, @NotNull final Node3D node )
+	{
+		return walkNode( visitor, Matrix3D.IDENTITY, node );
+	}
+
+	/**
+	 * Perform depth-first scene graph walk, starting at the given node with the
+	 * given transformation matrix.
+	 * <p>
+	 * The <code>visitor</code> is called for each visited node, including the
+	 * given <code>node</code>.
+	 *
+	 * @param   visitor     Visitor that will be called for each visited node.
+	 * @param   node        Root {@link Node3D} to start tree walk at.
+	 * @param   transform   Initial transformation matrix.
+	 *
+	 * @return  <code>true</code> if the tree walk was finished normally;
+	 *          <code>false</code> if the tree walk was aborted.
+	 */
+	public boolean walkNode( @NotNull final Node3DVisitor visitor, @NotNull final Matrix3D transform, @NotNull final Node3D node )
+	{
+		return walkNode( visitor, createPath( null, transform, node ) );
+	}
+
+	/**
+	 * Perform depth-first scene graph walk, starting at the given path element.
+	 * <p>
+	 * The <code>visitor</code> is called for each visited node, including the
+	 * given <code>node</code>.
+	 *
+	 * @param   visitor     Visitor that will be called for each visited node.
+	 * @param   path        Path element to walk from.
+	 *
+	 * @return  <code>true</code> if the tree walk was finished normally;
+	 *          <code>false</code> if the tree walk was aborted.
+	 */
+	public boolean walkNode( @NotNull final Node3DVisitor visitor, @NotNull final Node3DPath path )
+	{
 		boolean result = visitor.visitNode( path );
 		if ( result )
 		{
@@ -93,7 +157,7 @@ public class Node3DTreeWalker
 
 			for ( final Node3D child : node.getChildren() )
 			{
-				if ( !walk( visitor, createPath( path, transform, child ) ) )
+				if ( !walkNode( visitor, createPath( path, transform, child ) ) )
 				{
 					result = false;
 					break;
@@ -116,7 +180,7 @@ public class Node3DTreeWalker
 	 * @return  {@link Node3DPath} for the <code>node</code>.
 	 */
 	@NotNull
-	protected static Node3DPath createPath( @Nullable final Node3DPath parentPath, @NotNull final Matrix3D currentTransform, @NotNull final Node3D node )
+	protected Node3DPath createPath( @Nullable final Node3DPath parentPath, @NotNull final Matrix3D currentTransform, @NotNull final Node3D node )
 	{
 		return new Node3DPath( parentPath, getCombinedTransform( currentTransform, node ), node );
 	}
@@ -131,26 +195,19 @@ public class Node3DTreeWalker
 	 * @return  Combined transformation matrix for <code>node</code>.
 	 */
 	@NotNull
-	protected static Matrix3D getCombinedTransform( @NotNull final Matrix3D currentTransform, @NotNull final Node3D node )
+	protected Matrix3D getCombinedTransform( @NotNull final Matrix3D currentTransform, @NotNull final Node3D node )
 	{
 		Matrix3D result = currentTransform;
 
 		if ( node instanceof Transform3D )
 		{
 			final Matrix3D nodeTransform = ( (Transform3D) node ).getTransform();
-			if ( ( nodeTransform != null ) && ( nodeTransform != Matrix3D.IDENTITY ) )
+			if ( nodeTransform != Matrix3D.IDENTITY )
 			{
 				result = nodeTransform.multiply( result );
 			}
 		}
 
 		return result;
-	}
-
-	/**
-	 * Utility/Application class is not supposed to be instantiated.
-	 */
-	private Node3DTreeWalker()
-	{
 	}
 }
