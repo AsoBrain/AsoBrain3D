4c77bcff2c7ad61447eba8b7f166d0c0cea26a39
heijnen <heijnen>
2011-10-31 18:17:26 +0000

    Fixed copyright. Removed 3rd party library dependency. Small code maturity fixes.

--- java/ab/j3d/probe/ProbeApp.java
+++ java/ab/j3d/probe/ProbeApp.java
@@ -1,21 +1,28 @@
-/*
- * $Id$
+/* $Id$
+ * ====================================================================
+ * AsoBrain 3D Toolkit
+ * Copyright (C) 1999-2011 Peter S. Heijnen
  *
- * (C) Copyright Numdata BV 2009-2009 - All Rights Reserved
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This software may not be used, copied, modified, or distributed in any
- * form without express permission from Numdata BV. Please contact Numdata BV
- * for license information.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * ====================================================================
  */
 package ab.j3d.probe;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-import javax.swing.WindowConstants;
+import java.awt.*;
+import javax.swing.*;
 
-import com.numdata.oss.ui.WindowTools;
 
 /**
  * Runs the 3D capabilities probe as an application.
@@ -39,14 +46,18 @@ public class ProbeApp
 	public void run()
 	{
 		final ProbeUI ui = new ProbeUI();
-		ui.setPreferredSize( new Dimension( 800 , 400 ) );
+		ui.setPreferredSize( new Dimension( 800, 400 ) );
 
 		final JFrame frame = new JFrame( "3D Capabilities Probe" );
 		frame.setDefaultCloseOperation( WindowConstants.DISPOSE_ON_CLOSE );
 		frame.setLayout( new BorderLayout() );
-		frame.add( ui , BorderLayout.CENTER );
+		frame.add( ui, BorderLayout.CENTER );
 		frame.pack();
-		WindowTools.center( frame );
+		final Toolkit toolkit = frame.getToolkit();
+		final GraphicsConfiguration graphicsConfiguration = frame.getGraphicsConfiguration();
+		final Rectangle screenBounds = graphicsConfiguration.getBounds();
+		final Insets screenInsets = toolkit.getScreenInsets( graphicsConfiguration );
+		frame.setLocation( screenBounds.x + ( screenBounds.width + screenInsets.left + screenInsets.right - frame.getWidth() ) / 2, screenBounds.y + ( screenBounds.height + screenInsets.top + screenInsets.bottom - frame.getHeight() ) / 2 );
 		frame.setVisible( true );
 	}
 
