1a6f57b04cfb97b70a79bc0e9f421e22b681da62
heijnen <heijnen>
2011-11-01 23:12:51 +0000

    Remove AWT-dependencies from core AsoBrain 3D library:  - AWT-based code is moved to a new 'ab.j3d.awt' package (this is ongoing).  - The 'Vector3D' class now extends 'Vector2D' instead of 'Point2D' (remove AWT-dependency).  - Replaced 'Point2D.Float' usages with 'Vector2f'. - Replaced 'Point2D(.Double)' usages with 'Vector2D'.  - Replaced 'Color' with 'Color4f'.  - Only keep a very basic 2D shape definition in 'Contour' class.

--- test/ab/j3d/view/TestViewControlInput.java
+++ test/ab/j3d/view/TestViewControlInput.java
@@ -1,6 +1,7 @@
 /* $Id$
  * ====================================================================
- * (C) Copyright Numdata BV 2007-2009
+ * AsoBrain 3D Toolkit
+ * Copyright (C) 1999-2011 Peter S. Heijnen
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -19,27 +20,18 @@
  */
 package ab.j3d.view;
 
-import java.awt.Color;
-import java.awt.event.MouseEvent;
-import java.util.EventObject;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
 import java.util.List;
-import javax.swing.JPanel;
+import javax.swing.*;
 
-import junit.framework.TestCase;
-
-import ab.j3d.Material;
-import ab.j3d.Matrix3D;
-import ab.j3d.Vector3D;
-import ab.j3d.control.ControlInputEvent;
-import ab.j3d.geom.BasicRay3D;
-import ab.j3d.model.Face3DIntersection;
-import ab.j3d.model.Object3D;
-import ab.j3d.model.Object3DBuilder;
-import ab.j3d.model.Scene;
-import ab.j3d.view.java2d.Java2dView;
-
-import com.numdata.oss.event.EventDispatcher;
-import com.numdata.oss.event.EventFilter;
+import ab.j3d.*;
+import ab.j3d.awt.view.java2d.*;
+import ab.j3d.control.*;
+import ab.j3d.geom.*;
+import ab.j3d.model.*;
+import junit.framework.*;
 
 /**
  * This class tests the {@link ViewControlInput} class.
@@ -68,17 +60,17 @@ public class TestViewControlInput
 	public static Object3D createPlane( final double size )
 	{
 		final double halfSize = size / 2.0;
-		final Vector3D lf = Vector3D.INIT.set( -halfSize , -halfSize , 0.0 );
-		final Vector3D rf = Vector3D.INIT.set(  halfSize , -halfSize , 0.0 );
-		final Vector3D rb = Vector3D.INIT.set(  halfSize ,  halfSize , 0.0 );
-		final Vector3D lb = Vector3D.INIT.set( -halfSize ,  halfSize , 0.0 );
+		final Vector3D lf = new Vector3D( -halfSize, -halfSize, 0.0 );
+		final Vector3D rf = new Vector3D(  halfSize, -halfSize, 0.0 );
+		final Vector3D rb = new Vector3D(  halfSize,  halfSize, 0.0 );
+		final Vector3D lb = new Vector3D( -halfSize,  halfSize, 0.0 );
 
 		final Material red   = new Material( Color.RED  .getRGB() );
 		final Material green = new Material( Color.GREEN.getRGB() );
 
 		final Object3DBuilder builder = new Object3DBuilder();
-		/* top    */builder.addFace( new Vector3D[]{ lf , lb , rb , rf } , red   , false , false ); // Z =  size
-		/* bottom */builder.addFace( new Vector3D[]{ lb , lf , rf , rb } , green , false , false ); // Z = -size
+		/* top    */builder.addFace( new Vector3D[]{ lf, lb, rb, rf }, red, false, false ); // Z =  size
+		/* bottom */builder.addFace( new Vector3D[]{ lb, lf, rf, rb }, green, false, false ); // Z = -size
 		return builder.getObject3D();
 	}
 
@@ -103,73 +95,73 @@ public class TestViewControlInput
 		final Object3D plane6 = createPlane( 100.0 );
 		final Object3D plane7 = createPlane( 100.0 );
 
-		final Matrix3D transform1 = Matrix3D.getTransform(  90.0 ,  0.0 , 0.0 ,    0.0 ,   0.0 , 0.0 );
-		final Matrix3D transform2 = Matrix3D.getTransform(  90.0 ,  0.0 , 0.0 ,   10.0 ,  -1.0 , 0.0 );
-		final Matrix3D transform3 = Matrix3D.getTransform(  90.0 ,  0.0 , 0.0 ,  -10.0 , -20.0 , 0.0 );
-		final Matrix3D transform4 = Matrix3D.getTransform(  45.0 , 90.0 , 0.0 , -150.0 ,   0.0 , 0.0 );
-		final Matrix3D transform5 = Matrix3D.getTransform( -45.0 , 90.0 , 0.0 , -150.0 ,   0.0 , 0.0 );
-		final Matrix3D transform6 = Matrix3D.getTransform(  90.0 ,  0.0 , 0.0 ,  150.0 ,   0.0 , 0.0 );
-		final Matrix3D transform7 = Matrix3D.getTransform(   0.0 , 90.0 , 0.0 ,  150.0 ,   0.0 , 0.0 );
+		final Matrix3D transform1 = Matrix3D.getTransform(  90.0,  0.0, 0.0,    0.0,   0.0, 0.0 );
+		final Matrix3D transform2 = Matrix3D.getTransform(  90.0,  0.0, 0.0,   10.0,  -1.0, 0.0 );
+		final Matrix3D transform3 = Matrix3D.getTransform(  90.0,  0.0, 0.0,  -10.0, -20.0, 0.0 );
+		final Matrix3D transform4 = Matrix3D.getTransform(  45.0, 90.0, 0.0, -150.0,   0.0, 0.0 );
+		final Matrix3D transform5 = Matrix3D.getTransform( -45.0, 90.0, 0.0, -150.0,   0.0, 0.0 );
+		final Matrix3D transform6 = Matrix3D.getTransform(  90.0,  0.0, 0.0,  150.0,   0.0, 0.0 );
+		final Matrix3D transform7 = Matrix3D.getTransform(   0.0, 90.0, 0.0,  150.0,   0.0, 0.0 );
 
 		final Scene scene = new Scene( Scene.MM );
-		scene.addContentNode( "Plane 1" , transform1 , plane1 );
-		scene.addContentNode( "Plane 2" , transform2 , plane2 );
-		scene.addContentNode( "Plane 3" , transform3 , plane3 );
-		scene.addContentNode( "Plane 4" , transform4 , plane4 );
-		scene.addContentNode( "Plane 5" , transform5 , plane5 );
-		scene.addContentNode( "Plane 6" , transform6 , plane6 );
-		scene.addContentNode( "Plane 7" , transform7 , plane7 );
+		scene.addContentNode( "Plane 1", transform1, plane1 );
+		scene.addContentNode( "Plane 2", transform2, plane2 );
+		scene.addContentNode( "Plane 3", transform3, plane3 );
+		scene.addContentNode( "Plane 4", transform4, plane4 );
+		scene.addContentNode( "Plane 5", transform5, plane5 );
+		scene.addContentNode( "Plane 6", transform6, plane6 );
+		scene.addContentNode( "Plane 7", transform7, plane7 );
 
 		/*
 		 * Perform tests.
 		 */
-		final ViewControlTestInput input = new ViewControlTestInput( new Java2dView( scene , null ) );
+		final ViewControlTestInput input = new ViewControlTestInput( new Java2dView( scene, null ) );
 
 		List<Face3DIntersection> selection;
 
-		selection = input.getIntersections( new BasicRay3D( -45.0 , -500.0 , 0.0 , 0.0 , 1.0 , 0.0, true ) );
-		assertEquals( "Incorrect number of intersected faces;" , 2 , selection.size() );
-		assertEquals( "The planes are not listed from front to back at pos #0" , "Plane 3" , selection.get( 0 ).getObjectID() );
-		assertEquals( "The planes are not listed from front to back at pos #1" , "Plane 1" , selection.get( 1 ).getObjectID() );
+		selection = input.getIntersections( new BasicRay3D( -45.0, -500.0, 0.0, 0.0, 1.0, 0.0, true ) );
+		assertEquals( "Incorrect number of intersected faces;", 2, selection.size() );
+		assertEquals( "The planes are not listed from front to back at pos #0", "Plane 3", selection.get( 0 ).getObjectID() );
+		assertEquals( "The planes are not listed from front to back at pos #1", "Plane 1", selection.get( 1 ).getObjectID() );
 
-		selection = input.getIntersections( new BasicRay3D( 45.0 , -500.0 , 0.0 , 0.0 , 1.0 , 0.0, true ) );
-		assertEquals( "Incorrect number of intersected faces;" , 2 , selection.size() );
-		assertEquals( "The planes are not listed from front to back at pos #0" , "Plane 2" , selection.get( 0 ).getObjectID() );
-		assertEquals( "The planes are not listed from front to back at pos #1" , "Plane 1" , selection.get( 1 ).getObjectID() );
+		selection = input.getIntersections( new BasicRay3D( 45.0, -500.0, 0.0, 0.0, 1.0, 0.0, true ) );
+		assertEquals( "Incorrect number of intersected faces;", 2, selection.size() );
+		assertEquals( "The planes are not listed from front to back at pos #0", "Plane 2", selection.get( 0 ).getObjectID() );
+		assertEquals( "The planes are not listed from front to back at pos #1", "Plane 1", selection.get( 1 ).getObjectID() );
 
-		selection = input.getIntersections( new BasicRay3D( 0.0 , -500.0 , 0.0 , 0.0 , 1.0 , 0.0, true ) );
-		assertEquals( "Incorrect number of intersected faces;" , 3 , selection.size() );
-		assertEquals( "The planes are not listed from front to back at pos #0" , "Plane 3" , selection.get( 0 ).getObjectID() );
-		assertEquals( "The planes are not listed from front to back at pos #1" , "Plane 2" , selection.get( 1 ).getObjectID() );
-		assertEquals( "The planes are not listed from front to back at pos #1" , "Plane 1" , selection.get( 2 ).getObjectID() );
+		selection = input.getIntersections( new BasicRay3D( 0.0, -500.0, 0.0, 0.0, 1.0, 0.0, true ) );
+		assertEquals( "Incorrect number of intersected faces;", 3, selection.size() );
+		assertEquals( "The planes are not listed from front to back at pos #0", "Plane 3", selection.get( 0 ).getObjectID() );
+		assertEquals( "The planes are not listed from front to back at pos #1", "Plane 2", selection.get( 1 ).getObjectID() );
+		assertEquals( "The planes are not listed from front to back at pos #1", "Plane 1", selection.get( 2 ).getObjectID() );
 
-		selection = input.getIntersections( new BasicRay3D( -125.0 , -500.0 , 0.0 , 0.0 , 1.0 , 0.0, true ) );
-		assertEquals( "Incorrect number of intersected faces;" , 2 , selection.size() );
-		assertEquals( "The planes are not listed from front to back at pos #0" , "Plane 4" , selection.get( 0 ).getObjectID() );
-		assertEquals( "The planes are not listed from front to back at pos #1" , "Plane 5" , selection.get( 1 ).getObjectID() );
+		selection = input.getIntersections( new BasicRay3D( -125.0, -500.0, 0.0, 0.0, 1.0, 0.0, true ) );
+		assertEquals( "Incorrect number of intersected faces;", 2, selection.size() );
+		assertEquals( "The planes are not listed from front to back at pos #0", "Plane 4", selection.get( 0 ).getObjectID() );
+		assertEquals( "The planes are not listed from front to back at pos #1", "Plane 5", selection.get( 1 ).getObjectID() );
 
-		selection = input.getIntersections( new BasicRay3D( -149.9 , -500.0 , 0.0 , 0.0 , 1.0 , 0.0, true ) );
-		assertEquals( "Incorrect number of intersected faces;" , 2 , selection.size() );
-		assertEquals( "The planes are not listed from front to back at pos #0" , "Plane 4" , selection.get( 0 ).getObjectID() );
-		assertEquals( "The planes are not listed from front to back at pos #1" , "Plane 5" , selection.get( 1 ).getObjectID() );
+		selection = input.getIntersections( new BasicRay3D( -149.9, -500.0, 0.0, 0.0, 1.0, 0.0, true ) );
+		assertEquals( "Incorrect number of intersected faces;", 2, selection.size() );
+		assertEquals( "The planes are not listed from front to back at pos #0", "Plane 4", selection.get( 0 ).getObjectID() );
+		assertEquals( "The planes are not listed from front to back at pos #1", "Plane 5", selection.get( 1 ).getObjectID() );
 
-		selection = input.getIntersections( new BasicRay3D( -150.0 , -500.0 , 0.0 , 0.0 , 1.0 , 0.0, true ) );
-		assertEquals( "Incorrect number of intersected faces;" , 2 , selection.size() );
+		selection = input.getIntersections( new BasicRay3D( -150.0, -500.0, 0.0, 0.0, 1.0, 0.0, true ) );
+		assertEquals( "Incorrect number of intersected faces;", 2, selection.size() );
 
-		selection = input.getIntersections( new BasicRay3D( 200.0 , -25.0 , 0.0 , -Math.sqrt( 0.5 ) , Math.sqrt( 0.5 ) , 0.0, true ) );
-		assertEquals( "Incorrect number of intersected faces;" , 2 , selection.size() );
-		assertEquals( "The planes are not listed from front to back at pos #0" , "Plane 6" , selection.get( 0 ).getObjectID() );
-		assertEquals( "The planes are not listed from front to back at pos #1" , "Plane 7" , selection.get( 1 ).getObjectID() );
+		selection = input.getIntersections( new BasicRay3D( 200.0, -25.0, 0.0, -Math.sqrt( 0.5 ), Math.sqrt( 0.5 ), 0.0, true ) );
+		assertEquals( "Incorrect number of intersected faces;", 2, selection.size() );
+		assertEquals( "The planes are not listed from front to back at pos #0", "Plane 6", selection.get( 0 ).getObjectID() );
+		assertEquals( "The planes are not listed from front to back at pos #1", "Plane 7", selection.get( 1 ).getObjectID() );
 
-		selection = input.getIntersections( new BasicRay3D( 100.0 , 0.0 , -25.0 , 1.0 , 0.0 , 0.0, true ) );
-		assertEquals( "The number of intersected faces is not 2, but " + selection.size() , 1 , selection.size() );
-		assertEquals( "The planes are not listed from front to back at pos #0" , "Plane 7" , selection.get( 0 ).getObjectID() );
+		selection = input.getIntersections( new BasicRay3D( 100.0, 0.0, -25.0, 1.0, 0.0, 0.0, true ) );
+		assertEquals( "The number of intersected faces is not 2, but " + selection.size(), 1, selection.size() );
+		assertEquals( "The planes are not listed from front to back at pos #0", "Plane 7", selection.get( 0 ).getObjectID() );
 
-		selection = input.getIntersections( new BasicRay3D( 100.0 , 0.0 , -100.0 , 1.0 , 0.0 , 0.0, true ) );
-		assertEquals( "Incorrect number of intersected faces;" , 0 , selection.size() );
+		selection = input.getIntersections( new BasicRay3D( 100.0, 0.0, -100.0, 1.0, 0.0, 0.0, true ) );
+		assertEquals( "Incorrect number of intersected faces;", 0, selection.size() );
 
-		selection = input.getIntersections( new BasicRay3D( -60.1  , 0.0 , -500.0, 0.0 , 0.0 , 1.0, true ) );
-		assertEquals( "Incorrect number of intersected faces;" , 0 , selection.size() );
+		selection = input.getIntersections( new BasicRay3D( -60.1, 0.0, -500.0, 0.0, 0.0, 1.0, true ) );
+		assertEquals( "Incorrect number of intersected faces;", 0, selection.size() );
 	}
 
 	/**
@@ -188,38 +180,38 @@ public class TestViewControlInput
 		final Object3D plane1 = createPlane( 100.0 );
 		final Object3D plane2 = createPlane( 100.0 );
 
-		final Matrix3D transform1 = Matrix3D.getTransform( 90.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0   );
-		final Matrix3D transform2 = Matrix3D.getTransform( 0.0  , 0.0 , 0.0 , 0.0 , 0.0 , -75.0 );
+		final Matrix3D transform1 = Matrix3D.getTransform( 90.0, 0.0, 0.0, 0.0, 0.0, 0.0   );
+		final Matrix3D transform2 = Matrix3D.getTransform( 0.0, 0.0, 0.0, 0.0, 0.0, -75.0 );
 
 		final Scene scene = new Scene( Scene.MM );
-		scene.addContentNode( "Plane 1" , transform1 , plane1 );
-		scene.addContentNode( "Plane 2" , transform2 , plane2 );
+		scene.addContentNode( "Plane 1", transform1, plane1 );
+		scene.addContentNode( "Plane 2", transform2, plane2 );
 
 		/*
 		 * Perform tests.
 		 */
-		final ViewControlTestInput input = new ViewControlTestInput( new Java2dView( scene , null ) );
+		final ViewControlTestInput input = new ViewControlTestInput( new Java2dView( scene, null ) );
 
 		int modifiers = MouseEvent.BUTTON1_DOWN_MASK | MouseEvent.SHIFT_DOWN_MASK;
-		MouseEvent e = new MouseEvent( new JPanel() , MouseEvent.MOUSE_PRESSED , 0L , modifiers , 0 , 0 , 1 , false , MouseEvent.BUTTON1 );
+		MouseEvent e = new MouseEvent( new JPanel(), MouseEvent.MOUSE_PRESSED, 0L, modifiers, 0, 0, 1, false, MouseEvent.BUTTON1 );
 		input.mousePressed( e );
 
-		assertTrue("The last event is not a MouseControlEvent" , input.getLastEvent() instanceof ControlInputEvent );
+		assertTrue("The last event is not a MouseControlEvent", input.getLastEvent() instanceof ControlInputEvent );
 		ControlInputEvent event = (ControlInputEvent)input.getLastEvent();
-		assertSame( "Should get event back" , e , event.getInputEvent() );
+		assertSame( "Should get event back", e, event.getInputEvent() );
 
 		final int lastNumber = event.getSequenceNumber();
 
 		modifiers = MouseEvent.BUTTON1_DOWN_MASK | MouseEvent.CTRL_DOWN_MASK;
-		e = new MouseEvent( new JPanel() , MouseEvent.MOUSE_PRESSED , 0L , modifiers , 50 , 50 , 1 , false , MouseEvent.BUTTON2 );
+		e = new MouseEvent( new JPanel(), MouseEvent.MOUSE_PRESSED, 0L, modifiers, 50, 50, 1, false, MouseEvent.BUTTON2 );
 		input.mousePressed( e );
 
-		assertTrue( "The last event is not a MouseControlEvent" , input.getLastEvent() instanceof ControlInputEvent );
+		assertTrue( "The last event is not a MouseControlEvent", input.getLastEvent() instanceof ControlInputEvent );
 		event = (ControlInputEvent)input.getLastEvent();
 
-		assertSame( "Should get event back" , e , event.getInputEvent() );
+		assertSame( "Should get event back", e, event.getInputEvent() );
 
-		assertEquals( "The event number has changed" , lastNumber , event.getSequenceNumber() );
+		assertEquals( "The event number has changed", lastNumber, event.getSequenceNumber() );
 	}
 
 	/**
@@ -238,37 +230,37 @@ public class TestViewControlInput
 		final Object3D plane1 = createPlane( 100.0 );
 		final Object3D plane2 = createPlane( 100.0 );
 
-		final Matrix3D transform1 = Matrix3D.getTransform( 90.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0   );
-		final Matrix3D transform2 = Matrix3D.getTransform( 0.0  , 0.0 , 0.0 , 0.0 , 0.0 , -75.0 );
+		final Matrix3D transform1 = Matrix3D.getTransform( 90.0, 0.0, 0.0, 0.0, 0.0, 0.0   );
+		final Matrix3D transform2 = Matrix3D.getTransform( 0.0, 0.0, 0.0, 0.0, 0.0, -75.0 );
 
 		final Scene scene = new Scene( Scene.MM );
-		scene.addContentNode( "Plane 1" , transform1 , plane1 );
-		scene.addContentNode( "Plane 2" , transform2 , plane2 );
+		scene.addContentNode( "Plane 1", transform1, plane1 );
+		scene.addContentNode( "Plane 2", transform2, plane2 );
 
 		/*
 		 * Perform tests.
 		 */
-		final ViewControlTestInput input = new ViewControlTestInput( new Java2dView( scene , null ) );
+		final ViewControlTestInput input = new ViewControlTestInput( new Java2dView( scene, null ) );
 
 		int modifiers = MouseEvent.SHIFT_DOWN_MASK;
-		MouseEvent e = new MouseEvent( new JPanel() , MouseEvent.MOUSE_RELEASED , 0L , modifiers , 0 , 0 , 1 , false , MouseEvent.BUTTON1 );
+		MouseEvent e = new MouseEvent( new JPanel(), MouseEvent.MOUSE_RELEASED, 0L, modifiers, 0, 0, 1, false, MouseEvent.BUTTON1 );
 		input.mouseReleased( e );
 
-		assertTrue("The last event is not a MouseControlEvent" , input.getLastEvent() instanceof ControlInputEvent );
+		assertTrue("The last event is not a MouseControlEvent", input.getLastEvent() instanceof ControlInputEvent );
 		ControlInputEvent event = (ControlInputEvent)input.getLastEvent();
 
-		assertSame( "Should get event back" , e , event.getInputEvent() );
+		assertSame( "Should get event back", e, event.getInputEvent() );
 		final int lastNumber = event.getSequenceNumber();
 
 		modifiers = MouseEvent.CTRL_DOWN_MASK;
-		e = new MouseEvent( new JPanel() , MouseEvent.MOUSE_RELEASED , 0L , modifiers , 0 , 0 , 1 , false , MouseEvent.BUTTON3 );
+		e = new MouseEvent( new JPanel(), MouseEvent.MOUSE_RELEASED, 0L, modifiers, 0, 0, 1, false, MouseEvent.BUTTON3 );
 		input.mouseReleased( e );
 
-		assertTrue( "The last event is not a MouseControlEvent" , input.getLastEvent() instanceof ControlInputEvent );
+		assertTrue( "The last event is not a MouseControlEvent", input.getLastEvent() instanceof ControlInputEvent );
 		event = (ControlInputEvent)input.getLastEvent();
 
-		assertSame( "Should get event back" , e , event.getInputEvent() );
-		assertEquals( "The event number has not increased" , lastNumber + 1 , event.getSequenceNumber() );
+		assertSame( "Should get event back", e, event.getInputEvent() );
+		assertEquals( "The event number has not increased", lastNumber + 1, event.getSequenceNumber() );
 	}
 
 	/**
@@ -292,34 +284,36 @@ public class TestViewControlInput
 			super( view );
 
 			_lastEvent = null;
-			final EventDispatcher eventQueue = getEventDispatcher();
-			eventQueue.appendFilter( new EventFilter()
+
+			addControlInputListener( new ControlInputListener()
+			{
+				public void inputReceived( final ControlInputEvent event )
 				{
-					public EventObject filterEvent( final EventObject event )
-					{
-						_lastEvent = event;
-						return event;
-					}
-				} );
+					_lastEvent = event;
+				}
+			} );
 		}
 
+		@Override
 		public Projector getProjector()
 		{
-			return Projector.createInstance( ProjectionPolicy.PERSPECTIVE , 100 , 100 , 1.0 , Scene.M , 10.0 , 1000.0 , Math.toRadians( 45.0 ) , 1.0 );
+			return Projector.createInstance( ProjectionPolicy.PERSPECTIVE, 100, 100, 1.0, Scene.M, 10.0, 1000.0, Math.toRadians( 45.0 ), 1.0 );
 		}
 
+		@Override
 		public Matrix3D getScene2View()
 		{
-			return Matrix3D.INIT.set( 1.0 ,  0.0 , 0.0 ,    0.0 ,
-			                          0.0 ,  0.0 , 1.0 ,    0.0 ,
-			                          0.0 , -1.0 , 0.0 , -500.0 );
+			return new Matrix3D( 1.0,  0.0, 0.0,    0.0,
+			                     0.0,  0.0, 1.0,    0.0,
+			                     0.0, -1.0, 0.0, -500.0 );
 		}
 
+		@Override
 		public Matrix3D getView2Scene()
 		{
-			return Matrix3D.INIT.set( 1.0 , 0.0 ,  0.0 ,    0.0 ,
-			                          0.0 , 0.0 , -1.0 , -500.0 ,
-			                          0.0 , 1.0 ,  0.0 ,    0.0 );
+			return new Matrix3D( 1.0, 0.0,  0.0,    0.0,
+			                     0.0, 0.0, -1.0, -500.0,
+			                     0.0, 1.0,  0.0,    0.0 );
 		}
 
 		/**
