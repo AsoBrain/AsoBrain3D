03ccd4ee3ceb5308d8d9cd995e41ca88cbe87c1e
heijnen <heijnen>
2011-11-01 22:43:24 +0000

    Fix copyright/license. Code maturity fixes.

--- test/ab/j3d/geom/TestAbstractTriangle3D.java
+++ test/ab/j3d/geom/TestAbstractTriangle3D.java
@@ -1,58 +1,27 @@
-/* ====================================================================
- * $Id$
+/* $Id$
  * ====================================================================
- * Numdata Open Source Software License, Version 1.0
+ * AsoBrain 3D Toolkit
+ * Copyright (C) 1999-2011 Peter S. Heijnen
  *
- * Copyright (c) 2008-2009 Numdata BV.  All rights reserved.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *       "This product includes software developed by
- *        Numdata BV (http://www.numdata.com/)."
- *    Alternately, this acknowledgment may appear in the software itself,
- *    if and wherever such third-party acknowledgments normally appear.
- *
- * 4. The names "Numdata" must not be used to endorse or promote
- *    products derived from this software without prior written
- *    permission of Numdata BV. For written permission, please contact
- *    info@numdata.com.
- *
- * 5. Products derived from this software may not be called "Numdata",
- *    nor may "Numdata" appear in their name, without prior written
- *    permission of Numdata BV.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE NUMDATA BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  * ====================================================================
  */
 package ab.j3d.geom;
 
-import junit.framework.TestCase;
-
-import ab.j3d.Vector3D;
-
-import com.numdata.oss.MathTools;
+import ab.j3d.*;
+import junit.framework.*;
 
 /**
  * Test {@link BasicTriangle3D} class.
@@ -68,8 +37,10 @@ public class TestAbstractTriangle3D
 	 */
 	public void testTriangleMath()
 	{
-		/*
-		 * Create test class.
+		/**
+		 * Test properties.
+		 *
+		 * @noinspection JavaDoc
 		 */
 		class Test
 		{
@@ -80,7 +51,7 @@ public class TestAbstractTriangle3D
 			double _expectedPerimeter;
 			double _expectedArea;
 
-			Test( final Vector3D p1 , final Vector3D p2 , final Vector3D p3 , final double expectedPerimeter , final double expectedArea )
+			Test( final Vector3D p1, final Vector3D p2, final Vector3D p3, final double expectedPerimeter, final double expectedArea )
 			{
 				_p1 = p1;
 				_p2 = p2;
@@ -94,12 +65,12 @@ public class TestAbstractTriangle3D
 		/*
 		 * Create tests.
 		 */
-		final Test[] tests = new Test[]
+		final Test[] tests =
 			{
-				/* Test 1 */ new Test( Vector3D.INIT.set( 0.0 , 0.0 , 0.0 ) , Vector3D.INIT.set( 10.0 , 0.0 ,  0.0 ) , Vector3D.INIT.set( 10.0 , 10.0 ,  0.0 ) , 34.142 , 50.000 ) ,
-				/* Test 2 */ new Test( Vector3D.INIT.set( 0.0 , 0.0 , 0.0 ) , Vector3D.INIT.set( 10.0 , 0.0 ,  0.0 ) , Vector3D.INIT.set( 20.0 , 10.0 ,  0.0 ) , 46.502 , 49.999 ) ,
-				/* Test 3 */ new Test( Vector3D.INIT.set( 0.0 , 0.0 , 0.0 ) , Vector3D.INIT.set( 10.0 , 0.0 ,  0.0 ) , Vector3D.INIT.set(  5.0 , 10.0 ,  0.0 ) , 32.360 , 50.000 ) ,
-				/* Test 4 */ new Test( Vector3D.INIT.set( 0.0 , 0.0 , 0.0 ) , Vector3D.INIT.set(  0.0 , 0.0 , 10.0 ) , Vector3D.INIT.set(  0.0 , 10.0 , 10.0 ) , 34.142 , 50.000 ) ,
+				/* Test 1 */ new Test( Vector3D.ZERO, new Vector3D( 10.0, 0.0,  0.0 ), new Vector3D( 10.0, 10.0,  0.0 ), 34.142, 50.000 ),
+				/* Test 2 */ new Test( Vector3D.ZERO, new Vector3D( 10.0, 0.0,  0.0 ), new Vector3D( 20.0, 10.0,  0.0 ), 46.502, 49.999 ),
+				/* Test 3 */ new Test( Vector3D.ZERO, new Vector3D( 10.0, 0.0,  0.0 ), new Vector3D(  5.0, 10.0,  0.0 ), 32.360, 50.000 ),
+				/* Test 4 */ new Test( Vector3D.ZERO, new Vector3D(  0.0, 0.0, 10.0 ), new Vector3D(  0.0, 10.0, 10.0 ), 34.142, 50.000 ),
 			};
 
 		/*
@@ -111,11 +82,11 @@ public class TestAbstractTriangle3D
 		{
 			final String desc = "Test #" + ( testNr++ ) + ": ";
 
-			final double actualPerimeter = AbstractTriangle3D.calculatePerimeter( test._p1 , test._p2 , test._p3 );
-			assertTrue( desc + "Invalid perimeter calculated. Expected: " + test._expectedPerimeter + " Actual: " + actualPerimeter , MathTools.almostEqual( test._expectedPerimeter , actualPerimeter ) );
+			final double actualPerimeter = AbstractTriangle3D.calculatePerimeter( test._p1, test._p2, test._p3 );
+			assertTrue( desc + "Invalid perimeter calculated. Expected: " + test._expectedPerimeter + " Actual: " + actualPerimeter, MathTools.almostEqual( test._expectedPerimeter, actualPerimeter ) );
 
-			final double actualArea = AbstractTriangle3D.calculateArea( test._p1 , test._p2 , test._p3 );
-			assertTrue( desc + "Invalid area calculated. Expected: " + test._expectedArea + " Actual: " + actualArea , MathTools.almostEqual( test._expectedArea , actualArea ) );
+			final double actualArea = AbstractTriangle3D.calculateArea( test._p1, test._p2, test._p3 );
+			assertTrue( desc + "Invalid area calculated. Expected: " + test._expectedArea + " Actual: " + actualArea, MathTools.almostEqual( test._expectedArea, actualArea ) );
 		}
 	}
 }
