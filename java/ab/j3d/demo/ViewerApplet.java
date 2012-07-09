a0cfb33654ba9c30b9bda7df698a18d20938229e
heijnen <heijnen>
2012-07-09 09:35:23 +0000

    Added more logging and verbosity.

--- java/ab/j3d/demo/ViewerApplet.java
+++ java/ab/j3d/demo/ViewerApplet.java
@@ -78,128 +78,27 @@ public class ViewerApplet
 	/**
 	 * 3D view.
 	 */
-	private View3D _view;
+	private View3D _view = null;
 
 	/**
 	 * Scene to be viewed.
 	 */
-	private Scene _scene;
+	private Scene _scene = null;
 
 	/**
 	 * Executor for loading models asynchronously.
 	 */
-	private ExecutorService _executor;
+	private ExecutorService _executor = null;
 
 	/**
 	 * Shows status messages on top of the 3D view.
 	 */
-	private StatusOverlay _statusOverlay;
+	private StatusOverlay _statusOverlay = null;
 
 	@Override
 	public void init()
 	{
-		SwingUtilities.invokeLater( new Runnable()
-		{
-			public void run()
-			{
-				initOnEDT();
-			}
-		} );
-	}
-
-	/**
-	 * Performs initialization of the applet to be performed on the AWT event
-	 * dispatch thread, such as creation of Swing components.
-	 */
-	protected void initOnEDT()
-	{
-		final Locale locale = getLocale();
-		final ResourceBundle bundle = ResourceBundle.getBundle( BUNDLE_NAME, locale );
-
-		try
-		{
-			final Scene scene = new Scene( Scene.MM );
-			Scene.addLegacyLights( scene );
-			_scene = scene;
-
-			final RenderEngine engine;
-
-			if ( Boolean.parseBoolean( getParameter( "opengl" ) ) )
-			{
-				engine = new JOGLEngine();
-			}
-			else
-			{
-				engine = new Java2dEngine();
-			}
-
-			if ( engine instanceof JOGLEngine )
-			{
-				if ( Boolean.parseBoolean( getParameter( "shadows" ) ) )
-				{
-					final JOGLConfiguration configuration = ( (JOGLEngine)engine ).getConfiguration();
-					configuration.setShadowEnabled( true );
-					configuration.setShadowMultisampleEnabled( true );
-				}
-			}
-
-			final View3D view = engine.createView( scene );
-			view.setCameraControl( new FromToCameraControl( view ) );
-
-			final String background = getParameter( "background" );
-			if ( background != null )
-			{
-				if ( COLOR_RGB_6.matcher( background ).matches() )
-				{
-					view.setBackground( Background.createSolid( parseRgb6( background, 0 ) ) );
-				}
-				else if ( GRADIENT_2_RGB_6.matcher( background ).matches() )
-				{
-					final Color4f color1 = parseRgb6( background, 0 );
-					final Color4f color2 = parseRgb6( background, 8 );
-					view.setBackground( Background.createGradient( color1, color1, color2, color2 ) );
-				}
-				else
-				{
-					System.out.println( "Ignoring invalid background parameter: " + background );
-				}
-			}
-
-			final StatusOverlay statusOverlay = new StatusOverlay();
-			String statusText = "Loading model...";
-			try
-			{
-				statusText = bundle.getString( "loadingModel" );
-			}
-			catch ( MissingResourceException e )
-			{
-				/* ignored, will return default value */
-			}
-			statusOverlay.setStatus( statusText );
-			view.addOverlay( statusOverlay );
-			_statusOverlay = statusOverlay;
-
-			_view = view;
-
-			if ( Boolean.parseBoolean( getParameter( "controls" ) ) )
-			{
-				final JPanel panel = new JPanel( new BorderLayout() );
-				panel.add( view.getComponent() );
-				panel.add( View3DPanel.createToolBar( view, locale ), BorderLayout.PAGE_END );
-				setContentPane( panel );
-			}
-			else
-			{
-				add( view.getComponent() );
-			}
-
-			_executor = Executors.newSingleThreadExecutor();
-		}
-		catch ( Throwable e )
-		{
-			e.printStackTrace();
-			_statusOverlay.setStatus( e.toString() );
-		}
+		SwingUtilities.invokeLater( new InitOnEDT() );
 	}
 
 	/**
@@ -222,15 +121,30 @@ public class ViewerApplet
 	@Override
 	public void destroy()
 	{
-		SwingUtilities.invokeLater( new Runnable()
+		if ( _view != null )
 		{
-			public void run()
+			SwingUtilities.invokeLater( new Runnable()
 			{
-				_view.dispose();
-			}
-		} );
+				public void run()
+				{
+					_view.dispose();
+				}
+			} );
+		}
+		else
+		{
+			System.err.println( "destroy() called before view was made" );
+		}
 
-		_executor.shutdownNow();
+		final ExecutorService executor = _executor;
+		if ( executor != null )
+		{
+			executor.shutdownNow();
+		}
+		else
+		{
+			System.err.println( "destroy() called before executor was made" );
+		}
 	}
 
 	@Override
@@ -238,64 +152,15 @@ public class ViewerApplet
 	{
 		try
 		{
-			_executor.submit( new Runnable()
+			final ExecutorService executor = _executor;
+			if ( executor == null )
 			{
-				public void run()
-				{
-					final Object3D model;
-					try
-					{
-						final MultiResourceLoader resourceLoader = new MultiResourceLoader();
-
-						final URL source = new URL( getParameter( "source" ) );
-						resourceLoader.mount( new ZipResourceLoader( source ) );
-
-						if ( getParameter( "textures" ) != null )
-						{
-							final URL textures = new URL( getParameter( "textures" ) );
-							resourceLoader.mount( new URLResourceLoader( textures ) );
-						}
-
-						final ObjLoader objLoader = new ObjLoader( Matrix3D.IDENTITY );
-						model = objLoader.load( resourceLoader, "model.obj" );
-					}
-					catch ( IOException e )
-					{
-						e.printStackTrace();
-						_statusOverlay.setStatus( e.toString() );
-						throw new RuntimeException( e );
-					}
-
-					if ( model != null )
-					{
-						SwingUtilities.invokeLater( new Runnable()
-						{
-							public void run()
-							{
-								final Scene scene = _scene;
-								scene.addContentNode( "model", Matrix3D.IDENTITY, model );
-
-								final View3D view = _view;
-
-								final Bounds3D sceneBounds = scene.getBounds();
-								if ( sceneBounds != null )
-								{
-									final Vector3D center = sceneBounds.center();
-									final double fromX = ( sceneBounds.v2.x - center.x ) * 0.5 + center.x;
-									final double fromY = -1000.0;
-									final double fromZ = Math.max( 500.0, ( sceneBounds.v2.z - center.z ) * 0.5 + center.z );
-									view.setScene2View( Matrix3D.getFromToTransform( new Vector3D( fromX, fromY, fromZ ), center, Vector3D.POSITIVE_Z_AXIS, Vector3D.POSITIVE_Y_AXIS ) );
-									final CameraControl cameraControl = view.getCameraControl();
-									cameraControl.zoomToFit();
-									cameraControl.save();
-								}
-
-								_statusOverlay.setStatus( null );
-							}
-						} );
-					}
-				}
-			} );
+				System.err.println( "start() called before executor was made" );
+			}
+			else
+			{
+				executor.submit( new Start() );
+			}
 		}
 		catch ( Throwable e )
 		{
@@ -307,7 +172,7 @@ public class ViewerApplet
 	/**
 	 * Shows status text on top of 3D views.
 	 */
-	private class StatusOverlay
+	private static class StatusOverlay
 		implements ViewOverlay
 	{
 		/**
@@ -368,6 +233,195 @@ public class ViewerApplet
 				g.setColor( Color.WHITE );
 				g.drawString( status, (int)( (double)component.getWidth() - statusBounds.getWidth() ) / 2, fontMetrics.getLeading() + fontMetrics.getAscent() + padding );
 			}
+		}
+	}
+
+	/**
+	 * Performs initialization of the applet to be performed on the AWT event
+	 * dispatch thread, such as creation of Swing components.
+	 */
+	private class InitOnEDT
+		implements Runnable
+	{
+		public void run()
+		{
+			final Locale locale = getLocale();
+			final ResourceBundle bundle = ResourceBundle.getBundle( BUNDLE_NAME, locale );
+
+			try
+			{
+				final Scene scene = new Scene( Scene.MM );
+				Scene.addLegacyLights( scene );
+				_scene = scene;
+
+				final RenderEngine engine;
+
+				if ( Boolean.parseBoolean( getParameter( "opengl" ) ) )
+				{
+					engine = new JOGLEngine();
+				}
+				else
+				{
+					engine = new Java2dEngine();
+				}
+
+				if ( engine instanceof JOGLEngine )
+				{
+					if ( Boolean.parseBoolean( getParameter( "shadows" ) ) )
+					{
+						final JOGLEngine joglEngine = (JOGLEngine)engine;
+						final JOGLConfiguration configuration = joglEngine.getConfiguration();
+						configuration.setShadowEnabled( true );
+						configuration.setShadowMultisampleEnabled( true );
+					}
+				}
+				System.err.println( "Engine initialized" );
+
+				final View3D view = engine.createView( scene );
+				view.setCameraControl( new FromToCameraControl( view ) );
+
+				final String background = getParameter( "background" );
+				if ( background != null )
+				{
+					if ( COLOR_RGB_6.matcher( background ).matches() )
+					{
+						view.setBackground( Background.createSolid( parseRgb6( background, 0 ) ) );
+					}
+					else if ( GRADIENT_2_RGB_6.matcher( background ).matches() )
+					{
+						final Color4f color1 = parseRgb6( background, 0 );
+						final Color4f color2 = parseRgb6( background, 8 );
+						view.setBackground( Background.createGradient( color1, color1, color2, color2 ) );
+					}
+					else
+					{
+						System.out.println( "Ignoring invalid background parameter: " + background );
+					}
+				}
+
+				final StatusOverlay statusOverlay = new StatusOverlay();
+				String statusText = "Loading model...";
+				try
+				{
+					statusText = bundle.getString( "loadingModel" );
+				}
+				catch ( MissingResourceException e )
+				{
+					/* ignored, will return default value */
+				}
+				statusOverlay.setStatus( statusText );
+				view.addOverlay( statusOverlay );
+				_statusOverlay = statusOverlay;
+
+				_view = view;
+				System.err.println( "View initialized" );
+
+				if ( Boolean.parseBoolean( getParameter( "controls" ) ) )
+				{
+					final JPanel panel = new JPanel( new BorderLayout() );
+					panel.add( view.getComponent() );
+					panel.add( View3DPanel.createToolBar( view, locale ), BorderLayout.PAGE_END );
+					setContentPane( panel );
+				}
+				else
+				{
+					add( view.getComponent() );
+				}
+
+				_executor = Executors.newSingleThreadExecutor();
+				System.err.println( "Initialization complete" );
+			}
+			catch ( Throwable e )
+			{
+				System.err.println( "Initialization failed" );
+				e.printStackTrace();
+				_statusOverlay.setStatus( e.toString() );
+			}
+		}
+	}
+
+	/**
+	 * Called by {@link ViewerApplet#start()} to start the viewer.
+	 */
+	private class Start
+		implements Runnable
+	{
+		public void run()
+		{
+			final Scene scene = _scene;
+			final String source = getParameter( "source" );
+
+			Object3D model = null;
+			if ( scene == null )
+			{
+				System.err.println( "Can't start, because the scene was not initialized" );
+
+			}
+			else if ( source == null )
+			{
+				System.err.println( "No 'source' specified" );
+			}
+			else
+			{
+				try
+				{
+					final MultiResourceLoader resourceLoader = new MultiResourceLoader();
+
+					final URL sourceUrl = new URL( source );
+					resourceLoader.mount( new ZipResourceLoader( sourceUrl ) );
+
+					if ( getParameter( "textures" ) != null )
+					{
+						final URL textures = new URL( getParameter( "textures" ) );
+						resourceLoader.mount( new URLResourceLoader( textures ) );
+					}
+
+					final ObjLoader objLoader = new ObjLoader( Matrix3D.IDENTITY );
+					model = objLoader.load( resourceLoader, "model.obj" );
+				}
+				catch ( IOException e )
+				{
+					System.err.println( "Exception occurred while trying to load model" );
+					e.printStackTrace();
+					_statusOverlay.setStatus( e.toString() );
+					throw new RuntimeException( e );
+				}
+			}
+
+			if ( model != null )
+			{
+				scene.addContentNode( "model", Matrix3D.IDENTITY, model );
+
+				SwingUtilities.invokeLater( new StartOnEDT() );
+			}
+		}
+	}
+
+	/**
+	 * Called from {@link Start} on EDT to start the viewer.
+	 */
+	private class StartOnEDT
+		implements Runnable
+	{
+		public void run()
+		{
+
+			final View3D view = _view;
+			final Scene scene = _scene;
+			final Bounds3D sceneBounds = scene.getBounds();
+			if ( sceneBounds != null )
+			{
+				final Vector3D center = sceneBounds.center();
+				final double fromX = ( sceneBounds.v2.x - center.x ) * 0.5 + center.x;
+				final double fromY = -1000.0;
+				final double fromZ = Math.max( 500.0, ( sceneBounds.v2.z - center.z ) * 0.5 + center.z );
+				view.setScene2View( Matrix3D.getFromToTransform( new Vector3D( fromX, fromY, fromZ ), center, Vector3D.POSITIVE_Z_AXIS, Vector3D.POSITIVE_Y_AXIS ) );
+				final CameraControl cameraControl = view.getCameraControl();
+				cameraControl.zoomToFit();
+				cameraControl.save();
+			}
+
+			_statusOverlay.setStatus( null );
 		}
 	}
 }
