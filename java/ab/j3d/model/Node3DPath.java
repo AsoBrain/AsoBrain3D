1e410accedeeffdb57eb6b445a42898f03894bbd
Peter S. Heijnen <heijnen@numdata.com>
2013-03-05 14:13:12 +0100

    Added overloaded 'toString()' which supports an alternate string using specified separators.

--- java/ab/j3d/model/Node3DPath.java
+++ java/ab/j3d/model/Node3DPath.java
@@ -1,7 +1,6 @@
-/* $Id$
- * ====================================================================
+/*
  * AsoBrain 3D Toolkit
- * Copyright (C) 1999-2010 Peter S. Heijnen
+ * Copyright (C) 1999-2013 Peter S. Heijnen
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -16,7 +15,6 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- * ====================================================================
  */
 package ab.j3d.model;
 
@@ -27,13 +25,12 @@ import org.jetbrains.annotations.*;
  * Path to a {@link Node3D}, keeping track of the path leading upto this node
  * and the combined transformation matrix at the node.
  *
- * @author  Peter S. Heijnen
- * @version $Revision$ $Date$
+ * @author Peter S. Heijnen
  */
 public class Node3DPath
 {
 	/**
-	 * Parent path element. This is <code>null</code> for the 'root'.
+	 * Parent path element. This is {@code null} for the 'root'.
 	 */
 	@Nullable
 	private final Node3DPath _parent;
@@ -53,9 +50,9 @@ public class Node3DPath
 	/**
 	 * Create path element.
 	 *
-	 * @param   parent      Parent path element (<code>null</code> for root).
-	 * @param   transform   Combined transformation matrix at node.
-	 * @param   node        {@link Node3D} to which the path leads.
+	 * @param parent    Parent path element ({@code null} for root).
+	 * @param transform Combined transformation matrix at node.
+	 * @param node      {@link Node3D} to which the path leads.
 	 */
 	public Node3DPath( @Nullable final Node3DPath parent, @NotNull final Matrix3D transform, @NotNull final Node3D node )
 	{
@@ -65,10 +62,9 @@ public class Node3DPath
 	}
 
 	/**
-	 * Get parent path element. This is <code>null</code> for the 'root'.
+	 * Get parent path element. This is {@code null} for the 'root'.
 	 *
-	 * @return  Parent path element;
-	 *          <code>null</code> if this is the 'root'.
+	 * @return Parent path element; {@code null} if this is the 'root'.
 	 */
 	@Nullable
 	public Node3DPath getParent()
@@ -79,7 +75,7 @@ public class Node3DPath
 	/**
 	 * Get combined transformation matrix at the node.
 	 *
-	 * @return  Combined transformation matrix at the node.
+	 * @return Combined transformation matrix at the node.
 	 */
 	@NotNull
 	public Matrix3D getTransform()
@@ -90,7 +86,7 @@ public class Node3DPath
 	/**
 	 * Get {@link Node3D} to which this path leads.
 	 *
-	 * @return  {@link Node3D} to which this path leads.
+	 * @return {@link Node3D} to which this path leads.
 	 */
 	@NotNull
 	public Node3D getNode()
@@ -103,5 +99,20 @@ public class Node3DPath
 	{
 		final Class<?> clazz = getClass();
 		return clazz.getSimpleName() + '@' + Integer.toHexString( hashCode() ) + "{node=" + _node + ", transform=" + _transform.toShortFriendlyString() + ", parent=" + _parent + '}';
+	}
+
+	/**
+	 * Give string representation of path. This allows a separator to be specified
+	 * that separates the parent from this instance.
+	 *
+	 * @param separator Path element separator.
+	 *
+	 * @return String representation of path.
+	 */
+	public String toString( final String separator )
+	{
+		final Class<?> clazz = getClass();
+		final Node3DPath parent = _parent;
+		return ( parent != null ? parent.toString( separator ) + separator : "" ) + clazz.getSimpleName() + '@' + Integer.toHexString( hashCode() ) + "{node=" + _node + ", transform=" + _transform.toShortFriendlyString() + '}';
 	}
 }
