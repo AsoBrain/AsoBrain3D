8d6c2fc3fcce97b08f36813334dcc9c85dfeb05c
Gerrit Meinders <meinders@numdata.com>
2016-09-13 10:19:11 +0200

    Fixed typo: AsoBrain *2D* Toolkit ;)

--- java/ab/j3d/Vector2D.java
+++ java/ab/j3d/Vector2D.java
@@ -1,7 +1,6 @@
-/* $Id$
- * ====================================================================
- * AsoBrain 2D Toolkit
- * Copyright (C) 1999-2011 Peter S. Heijnen
+/*
+ * AsoBrain 3D Toolkit
+ * Copyright (C) 1999-2016 Peter S. Heijnen
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -16,7 +15,6 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- * ====================================================================
  */
 package ab.j3d;
 
@@ -30,7 +28,6 @@ import ab.j3d.geom.*;
  * This class represents a 2D vector.
  *
  * @author  Peter S. Heijnen
- * @version $Revision$ ($Date$, $Author$)
  */
 public class Vector2D
 	implements Serializable
