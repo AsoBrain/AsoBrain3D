35f5a60b3741655d2b7f7d43b8711804a5a81b9e
Gerrit Meinders <meinders@numdata.com>
2020-12-18 16:26:10 +0100

    'NodeUpdate.prepare' now has an empty default implementation, allowing simple node updates to be lambda expressions.

--- core/src/main/java/ab/j3d/model/NodeUpdate.java
+++ core/src/main/java/ab/j3d/model/NodeUpdate.java
@@ -1,8 +1,6 @@
 /*
- * $Id$
- * ====================================================================
  * AsoBrain 3D Toolkit
- * Copyright (C) 1999-2011 Peter S. Heijnen
+ * Copyright (C) 1999-2020 Peter S. Heijnen
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -17,7 +15,6 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- * ====================================================================
  */
 package ab.j3d.model;
 
@@ -41,7 +38,9 @@ public interface NodeUpdate
 	 * Makes preparations for a subsequent {@link #update(Scene)} call.
 	 * The scene and any nodes in the scene must not be affected.
 	 */
-	void prepare();
+	default void prepare()
+	{
+	}
 
 	/**
 	 * Updates the scene.
