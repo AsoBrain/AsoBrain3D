65346227e9199e9472f63585d682d5729451b619
Gerrit Meinders <meinders@numdata.com>
2021-03-30 16:15:34 +0200

    Add 'Vector2D' to module exports.

--- js/module.js
+++ js/module.js
@@ -1,6 +1,6 @@
 /*
  * AsoBrain 3D Toolkit
- * Copyright (C) 1999-2019 Peter S. Heijnen
+ * Copyright (C) 1999-2021 Peter S. Heijnen
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -17,46 +17,27 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
-import Bounds3D from './lib/ab.j3d/Bounds3D';
-import Bounds3DBuilder from './lib/ab.j3d/Bounds3DBuilder';
-import MathTools from './lib/ab.j3d/MathTools';
-import Matrix3D from './lib/ab.j3d/Matrix3D';
-import Scene from './lib/ab.j3d/Scene';
-import Vector3D from './lib/ab.j3d/Vector3D';
+export { default as Bounds3D } from './lib/ab.j3d/Bounds3D';
+export { default as Bounds3DBuilder } from './lib/ab.j3d/Bounds3DBuilder';
+export { default as Color4 } from './lib/ab.j3d/Color4';
+export { default as CubeMap } from './lib/ab.j3d/CubeMap';
+export { default as MathTools } from './lib/ab.j3d/MathTools';
+export { default as Matrix3D } from './lib/ab.j3d/Matrix3D';
+export { default as Scene } from './lib/ab.j3d/Scene';
+export { default as TextureMap } from './lib/ab.j3d/TextureMap';
+export { default as Vector2D } from './lib/ab.j3d/Vector2D';
+export { default as Vector3D } from './lib/ab.j3d/Vector3D';
 
-import BoxUVMap from './lib/ab.j3d.geom/BoxUVMap';
-import Contour from './lib/ab.j3d.geom/Contour';
-import GeometryTools from './lib/ab.j3d.geom/GeometryTools';
-import PlanarUVMap from './lib/ab.j3d.geom/PlanarUVMap';
-import QuadList from './lib/ab.j3d.geom/QuadList';
-import QuadStrip from './lib/ab.j3d.geom/QuadStrip';
-import TessellationPrimitive from './lib/ab.j3d.geom/TessellationPrimitive';
-import TransformUVGenerator from './lib/ab.j3d.geom/TransformUVGenerator';
-import TriangleFan from './lib/ab.j3d.geom/TriangleFan';
-import TriangleList from './lib/ab.j3d.geom/TriangleList';
-import TriangleStrip from './lib/ab.j3d.geom/TriangleStrip';
-import UVGenerator from './lib/ab.j3d.geom/UVGenerator';
-import UVMap from './lib/ab.j3d.geom/UVMap';
-
-export {
-	Bounds3D,
-	Bounds3DBuilder,
-	MathTools,
-	Matrix3D,
-	Scene,
-	Vector3D,
-
-	BoxUVMap,
-	Contour,
-	GeometryTools,
-	PlanarUVMap,
-	QuadList,
-	QuadStrip,
-	TessellationPrimitive,
-	TransformUVGenerator,
-	TriangleFan,
-	TriangleList,
-	TriangleStrip,
-	UVGenerator,
-	UVMap
-};
+export { default as BoxUVMap } from './lib/ab.j3d.geom/BoxUVMap';
+export { default as Contour } from './lib/ab.j3d.geom/Contour';
+export { default as GeometryTools } from './lib/ab.j3d.geom/GeometryTools';
+export { default as PlanarUVMap } from './lib/ab.j3d.geom/PlanarUVMap';
+export { default as QuadList } from './lib/ab.j3d.geom/QuadList';
+export { default as QuadStrip } from './lib/ab.j3d.geom/QuadStrip';
+export { default as TessellationPrimitive } from './lib/ab.j3d.geom/TessellationPrimitive';
+export { default as TransformUVGenerator } from './lib/ab.j3d.geom/TransformUVGenerator';
+export { default as TriangleFan } from './lib/ab.j3d.geom/TriangleFan';
+export { default as TriangleList } from './lib/ab.j3d.geom/TriangleList';
+export { default as TriangleStrip } from './lib/ab.j3d.geom/TriangleStrip';
+export { default as UVGenerator } from './lib/ab.j3d.geom/UVGenerator';
+export { default as UVMap } from './lib/ab.j3d.geom/UVMap';
